7aad42bc50b5cb2e94d67416ae6f95b0404662e1
ShezHsky <shezhsky@icloud.com>
2017-07-05 22:21:57 +0100

    Add initial ports to make/save events on calendar
    Based on authorisation state, requesting it as we go. May end up seperating authorisation and event DAO behaviours into seperate types later

--- EurofurenceTests/CalendarServiceTests.swift
+++ EurofurenceTests/CalendarServiceTests.swift
@@ -13,8 +13,14 @@ protocol CalendarPort {
     
     var isAuthorizedForEventsAccess: Bool { get }
 
-    func requestAccessToEvents()
+    func requestAccessToEvents(completionHandler: (Bool) -> Void)
+    func makeEvent() -> CalendarEvent
+    func save(event: CalendarEvent)
 
+}
+
+protocol CalendarEvent {
+    
 }
 
 class CapturingCalendarPort: CalendarPort {
@@ -26,10 +32,26 @@ class CapturingCalendarPort: CalendarPort {
     }
 
     private(set) var wasAskedForEventsPermissions = false
-    func requestAccessToEvents() {
+    func requestAccessToEvents(completionHandler: (Bool) -> Void) {
         wasAskedForEventsPermissions = true
     }
+    
+    private(set) var didMakeEvent = false
+    var createdEvent: CalendarEvent = StubCalendarEvent()
+    func makeEvent() -> CalendarEvent {
+        didMakeEvent = true
+        return createdEvent
+    }
+    
+    private(set) var savedEvent: CalendarEvent?
+    func save(event: CalendarEvent) {
+        savedEvent = event
+    }
 
+}
+
+class StubCalendarEvent: CalendarEvent {
+    
 }
 
 class AuthorizedCalendarPort: CapturingCalendarPort {
@@ -38,6 +60,34 @@ class AuthorizedCalendarPort: CapturingCalendarPort {
         get {
             return true
         }
+    }
+    
+}
+
+class UnauthorizedCalendarPort: CapturingCalendarPort {
+    
+    override var isAuthorizedForEventsAccess: Bool {
+        get {
+            return false
+        }
+    }
+    
+}
+
+class PermissionGrantingCalendarPort: CapturingCalendarPort {
+    
+    override func requestAccessToEvents(completionHandler: (Bool) -> Void) {
+        super.requestAccessToEvents(completionHandler: completionHandler)
+        completionHandler(true)
+    }
+    
+}
+
+class PermissionDenyingCalendarPort: CapturingCalendarPort {
+    
+    override func requestAccessToEvents(completionHandler: (Bool) -> Void) {
+        super.requestAccessToEvents(completionHandler: completionHandler)
+        completionHandler(false)
     }
     
 }
@@ -51,8 +101,16 @@ class CalendarService {
     }
 
     func add(event: Any) {
-        if !calendar.isAuthorizedForEventsAccess {
-            calendar.requestAccessToEvents()
+        if calendar.isAuthorizedForEventsAccess {
+            let calendarEvent = calendar.makeEvent()
+            calendar.save(event: calendarEvent)
+        }
+        else {
+            calendar.requestAccessToEvents() { authorized in
+                if authorized {
+                    _ = calendar.makeEvent()
+                }
+            }
         }
     }
 
@@ -83,6 +141,53 @@ class CalendarServiceTests: XCTestCase {
         service.add(event: event)
         
         XCTAssertFalse(capturingCalendar.wasAskedForEventsPermissions)
+    }
+    
+    func testHavingEventsPermissionsWhenAddingEventShouldRequestCreationOfCalendarEvent() {
+        let capturingCalendar = AuthorizedCalendarPort()
+        let service = CalendarService(calendar: capturingCalendar)
+        let event = Event()
+        service.add(event: event)
+        
+        XCTAssertTrue(capturingCalendar.didMakeEvent)
+    }
+    
+    func testHavingEventsPermissionsWhenAddingEventShouldTellTheCalendarToSaveItsCreatedEvent() {
+        let capturingCalendar = AuthorizedCalendarPort()
+        let service = CalendarService(calendar: capturingCalendar)
+        let event = Event()
+        let stubbedCalendarEvent = StubCalendarEvent()
+        capturingCalendar.createdEvent = stubbedCalendarEvent
+        service.add(event: event)
+        
+        XCTAssertTrue(stubbedCalendarEvent === capturingCalendar.savedEvent as? StubCalendarEvent)
+    }
+    
+    func testNotHavingEventsPermissionsShouldNotMakeEventFromCalendar() {
+        let capturingCalendar = UnauthorizedCalendarPort()
+        let service = CalendarService(calendar: capturingCalendar)
+        let event = Event()
+        service.add(event: event)
+        
+        XCTAssertFalse(capturingCalendar.didMakeEvent)
+    }
+    
+    func testRequestingPermissionWhenAddingEventThenBeingGrantedItShouldMakeTheEvent() {
+        let capturingCalendar = PermissionGrantingCalendarPort()
+        let service = CalendarService(calendar: capturingCalendar)
+        let event = Event()
+        service.add(event: event)
+        
+        XCTAssertTrue(capturingCalendar.didMakeEvent)
+    }
+    
+    func testRequestingPermissionWhenAddingEventThenBeingDeniedItShouldNotMakeTheEvent() {
+        let capturingCalendar = PermissionDenyingCalendarPort()
+        let service = CalendarService(calendar: capturingCalendar)
+        let event = Event()
+        service.add(event: event)
+        
+        XCTAssertFalse(capturingCalendar.didMakeEvent)
     }
     
 }
