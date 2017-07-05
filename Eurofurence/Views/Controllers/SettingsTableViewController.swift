40642a7b4445a6d22a08d69aec6601c6cc2be2b5
ShezHsky <shezhsky@icloud.com>
2017-07-05 20:56:03 +0100

    Tidy up Settings view controller
    - Deleted unused overrides
    - Split form construction into methods by section
    - Fixed typos

--- Eurofurence/Views/Controllers/SettingsTableViewController.swift
+++ Eurofurence/Views/Controllers/SettingsTableViewController.swift
@@ -9,12 +9,22 @@ import UIKit
 import Eureka
 
 class SettingsTableViewController: FormViewController {
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        makeNetworkSection()
+        makeDataStorageSection()
+        makeExperimentalFeaturesSection()
+        #if DEBUG
+            makeDebuggingSettingsSection()
+        #endif
+    }
+
+    private func makeNetworkSection() {
         form +++ Section("Network")
-            <<< SwitchRow("switchRowUpdateOnStart") { row in      // initializer
-                row.title = "Automaticaly update on start"
+            <<< SwitchRow("switchRowUpdateOnStart") { row in
+                row.title = "Auto-Update on Launch"
                 row.value = UserSettings.UpdateOnStart.currentValue()
                 }.onChange { row in
                     UserSettings.UpdateOnStart.setValue(row.value!)
@@ -22,8 +32,8 @@ class SettingsTableViewController: FormViewController {
                 }.cellUpdate { cell, row in
                     cell.textLabel?.font = UIFont.preferredFont(forTextStyle: UIFontTextStyle.body)
             }
-            <<< SwitchRow("SwitchRow") { row in      // initializer
-                row.title = "Auto-update on mobile"
+            <<< SwitchRow("SwitchRow") { row in
+                row.title = "Auto-Update on Mobile"
                 row.value = UserSettings.AutomaticRefreshOnMobile.currentValue()
                 }.onChange { row in
                     UserSettings.AutomaticRefreshOnMobile.setValue(row.value!)
@@ -31,12 +41,12 @@ class SettingsTableViewController: FormViewController {
                 }.cellUpdate { cell, row in
                     cell.textLabel?.font = UIFont.preferredFont(forTextStyle: UIFontTextStyle.body)
             }
-            <<< PushRow<Int>("pushRowRefreshTimer") { row in      // initializer
-                row.title = "Refresh interval"
-                
+            <<< PushRow<Int>("pushRowRefreshTimer") { row in
+                row.title = "Refresh Interval"
+
                 row.displayValueFor = { value in
                     var minutes = -1
-                    
+
                     if let value = value {
                         minutes = Int(value/60)
                         if value <= 0 {
@@ -44,20 +54,20 @@ class SettingsTableViewController: FormViewController {
                         }
                     }
                     if minutes == 1 {
-                        return "every 1 minute"
+                        return "Every 1 Minute"
                     } else {
-                        return "every " + String(minutes) + " minutes"
+                        return "Every " + String(minutes) + " Minutes"
                     }
                 }
-                
+
                 row.options = [-1, 300, 600, 900, 1800, 3600]
-                
+
                 row.value = UserSettings.RefreshTimer.currentValue()
                 }.onChange { row in
-                    
+
                     // TODO: BUG! Label becomes empty when currently selected entry is selected again
-                    
-					var refreshSeconds: Int = UserSettings.RefreshTimer.currentValueOrDefault()
+
+                    var refreshSeconds: Int = UserSettings.RefreshTimer.currentValueOrDefault()
                     if let value = row.value {
                         refreshSeconds = value
                     } else {
@@ -69,15 +79,15 @@ class SettingsTableViewController: FormViewController {
                     } else {
                         UIApplication.shared.setMinimumBackgroundFetchInterval(UIApplicationBackgroundFetchIntervalNever)
                     }
-					// TODO: Reimplement timed refresh (maybe X minutes after last push-triggered refresh?)
+                    // TODO: Reimplement timed refresh (maybe X minutes after last push-triggered refresh?)
                     /*AutomaticRefresh.sharedInstance.updateTimer()*/
                     row.updateCell()
                 }.cellUpdate { cell, row in
                     cell.textLabel?.font = UIFont.preferredFont(forTextStyle: UIFontTextStyle.body)
             }
 
-            <<< SwitchRow("switchRowNotifyOnAnnouncement") { row in      // initializer
-                row.title = "Notify on new announcements"
+            <<< SwitchRow("switchRowNotifyOnAnnouncement") { row in
+                row.title = "Notify on New Announcements"
                 row.value = UserSettings.NotifyOnAnnouncement.currentValue()
                 row.hidden = Condition.function(["pushRowRefreshTimer"], { form in
                     let value = (form.rowBy(tag: "pushRowRefreshTimer") as? PushRow<Int>)?.value
@@ -93,20 +103,23 @@ class SettingsTableViewController: FormViewController {
                     row.updateCell()
                 }.cellUpdate { cell, row in
                     cell.textLabel?.font = UIFont.preferredFont(forTextStyle: UIFontTextStyle.body)
-            }
-            +++ Section("Data Storage")
+        }
+    }
+
+    private func makeDataStorageSection() {
+        form +++ Section("Data Storage")
             <<< ButtonRow(){
-                $0.title = "Download database"
+                $0.title = "Download Database"
                 }.onCellSelection { row in
-					// TODO: Perform sync without since
+                    // TODO: Perform sync without since
                     /*ApiManager.sharedInstance.updateAllEntities(true, completion: nil)*/
                 }.cellUpdate { cell, row in
                     cell.textLabel?.font = UIFont.preferredFont(forTextStyle: UIFontTextStyle.body)
             }
             <<< ButtonRow(){
-                $0.title = "Clear database & cache"
+                $0.title = "Clear Database and Cache"
                 }.onCellSelection { row in
-					// TODO: Clear cache and storage
+                    // TODO: Clear cache and storage
                     /*ApiManager.sharedInstance.clearCache()*/
                 }.cellUpdate { cell, row in
                     cell.textLabel?.font = UIFont.preferredFont(forTextStyle: UIFontTextStyle.body)
@@ -114,13 +127,16 @@ class SettingsTableViewController: FormViewController {
             }
             +++ Section("Other")
             <<< LabelRow() { row in
-				// TODO: VersionProvider?
+                // TODO: VersionProvider?
                 row.title = "Version: " /*+ ConfigManager.sharedInstance.appVersion*/
                 }.cellUpdate { cell, row in
                     cell.textLabel?.font = UIFont.preferredFont(forTextStyle: UIFontTextStyle.subheadline)
-            }
-            +++ Section(header:"Experimental features", footer: "Allowing the app to try refreshing in background will only consume a small amount of data. This allows us to keep you updated on the latest announcements regarding delays and other important events at the con. Please note that background refreshing may not always work and can be unreliable!")
-            <<< SwitchRow("switchRowRefreshInBackground") { row in      // initializer
+        }
+    }
+
+    private func makeExperimentalFeaturesSection() {
+        form +++ Section(header:"Experimental Features", footer: "Allowing the app to try refreshing in background will only consume a small amount of data. This allows us to keep you updated on the latest announcements regarding delays and other important events at the con. Please note that background refreshing may not always work and can be unreliable.")
+            <<< SwitchRow("switchRowRefreshInBackground") { row in
                 row.title = "Refresh in background"
                 row.value = UserSettings.RefreshInBackground.currentValue()
                 }.onChange { row in
@@ -140,8 +156,8 @@ class SettingsTableViewController: FormViewController {
                     cell.textLabel?.font = UIFont.preferredFont(forTextStyle: UIFontTextStyle.subheadline)
                     cell.backgroundColor = UIColor.lightText
             }
-            <<< SwitchRow("switchRowRefreshInBackgroundOnMobile") { row in      // initializer
-                row.title = "Background refresh on mobile"
+            <<< SwitchRow("switchRowRefreshInBackgroundOnMobile") { row in
+                row.title = "Background Refresh on Mobile"
                 row.value = UserSettings.RefreshInBackgroundOnMobile.currentValue()
                 row.hidden = Condition.function(["pushRowRefreshTimer", "switchRowRefreshInBackground"], { form in
                     return !((form.rowBy(tag: "switchRowNotifyOnAnnouncement") as? SwitchRow)?.value ?? true) || !((form.rowBy(tag: "switchRowRefreshInBackground") as? SwitchRow)?.value ?? true)
@@ -152,11 +168,12 @@ class SettingsTableViewController: FormViewController {
                 }.cellUpdate { cell, row in
                     cell.textLabel?.font = UIFont.preferredFont(forTextStyle: UIFontTextStyle.subheadline)
                     cell.backgroundColor = UIColor.lightText
-			}
+        }
+    }
 
-#if DEBUG
-            form +++ Section(header:"Debugging", footer: "These settings are intended for debugging purposes only and may cause instability or unexpected behaviour if changed!")
-            <<< TimeIntervalRow("timeIntervalRowTimeOffset") { row in      // initializer
+    private func makeDebuggingSettingsSection() {
+        form +++ Section(header:"Debugging", footer: "These settings are intended for debugging purposes only and may cause instability or unexpected behaviour if changed!")
+            <<< TimeIntervalRow("timeIntervalRowTimeOffset") { row in
                 row.title = "Time Offset"
                 row.value = UserSettings.DebugTimeOffset.currentValue()
                 }.onChange { row in
@@ -167,32 +184,13 @@ class SettingsTableViewController: FormViewController {
                 }.cellUpdate { cell, row in
                     cell.textLabel?.font = UIFont.preferredFont(forTextStyle: UIFontTextStyle.subheadline)
                     cell.backgroundColor = UIColor.lightText
-            }
-#endif
+        }
+    }
 
-        // Do any additional setup after loading the view.
-    }
-	
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-	
-	
-    /*
-     // MARK: - Navigation
-	
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-     // Get the new view controller using segue.destinationViewController.
-     // Pass the selected object to the new view controller.
-     }
-     */
-    
     @IBAction func openMenu(_ sender: AnyObject) {
         showApp();
     }
-    
+
     func showApp() {
         let rootVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "MainTabBarController") as! UITabBarController
         self.slideMenuController()?.changeMainViewController(rootVC, close: true)
