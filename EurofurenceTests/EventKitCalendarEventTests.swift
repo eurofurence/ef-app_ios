6deb8c8f3599d76d8e5d2ac4ece9ee5e3dda6251
ShezHsky <shezhsky@icloud.com>
2017-07-07 06:31:27 +0100

    Cover remaining event adapter requirements with tests

--- EurofurenceTests/EventKitCalendarEventTests.swift
+++ EurofurenceTests/EventKitCalendarEventTests.swift
@@ -19,7 +19,8 @@ class EventKitCalendarEvent: CalendarEvent {
     }
     
     var isAssociatedToCalendar: Bool {
-        return false
+        let calendar: EKCalendar? = event.calendar
+        return calendar != nil
     }
     
     var title: String {
@@ -42,78 +43,128 @@ class EventKitCalendarEvent: CalendarEvent {
     
     var location: String? {
         get {
-            return nil
+            return event.location
         }
         set {
-            
+            event.location = newValue
         }
     }
     
     var startDate: Date {
         get {
-            return Date()
+            return event.startDate
         }
         set {
-            
+            event.startDate = newValue
         }
     }
     
     var endDate: Date {
         get {
-            return Date()
+            return event.endDate
         }
         set {
-            
+            event.endDate = newValue
         }
     }
     
     func addAlarm(relativeOffsetFromStartDate relativeOffset: TimeInterval) {
-        
+        let alarm = EKAlarm(relativeOffset: relativeOffset)
+        event.addAlarm(alarm)
     }
     
 }
 
 class EventKitCalendarEventTests: XCTestCase {
     
-    private func makeTestEvent() -> EKEvent {
-        let eventStore = EKEventStore()
-        let event = EKEvent(eventStore: eventStore)
+    var eventStore: EKEventStore!
+    var event: EKEvent!
+    var adapter: EventKitCalendarEvent!
+    
+    override func setUp() {
+        super.setUp()
+        
+        eventStore = EKEventStore()
+        event = EKEvent(eventStore: eventStore)
         event.title = "Some Title"
         event.notes = "Some Notes"
+        event.location = "Some Location"
+        event.startDate = .distantPast
+        event.endDate = .distantFuture
         
-        return event
+        adapter = EventKitCalendarEvent(event: event)
+    }
+    
+    func testTheEventShouldBeAssociatedToCalendarIfItHasOne() {
+        event.calendar = EKCalendar(for: .event, eventStore: eventStore)
+        
+        XCTAssertTrue(adapter.isAssociatedToCalendar)
+    }
+    
+    func testTheEventShouldNotBeAssociatedToCalendarIfItDoesNotHaveOne() {
+        XCTAssertFalse(adapter.isAssociatedToCalendar)
     }
     
     func testAccessingTheEventTitleShouldReturnTheTitleFromTheUnderlyingEvent() {
-        let event = makeTestEvent()
-        let adapter = EventKitCalendarEvent(event: event)
-        
         XCTAssertEqual(event.title, adapter.title)
     }
     
     func testSettingTheEventTitleShouldUpdateTheTitleOnTheUnderlyingEvent() {
-        let event = makeTestEvent()
-        let adapter = EventKitCalendarEvent(event: event)
         let expectedTitle = "Some other title"
         adapter.title = expectedTitle
         
-        XCTAssertEqual(expectedTitle, adapter.title)
+        XCTAssertEqual(expectedTitle, event.title)
     }
     
     func testAccessingTheEventNotesShouldReturnTheNotesFromTheUnderlyingEvent() {
-        let event = makeTestEvent()
-        let adapter = EventKitCalendarEvent(event: event)
-        
         XCTAssertEqual(event.notes, adapter.notes)
     }
     
     func testSettingTheEventNotesShouldUpdateTheNotesOnTheUnderlyingEvent() {
-        let event = makeTestEvent()
-        let adapter = EventKitCalendarEvent(event: event)
         let expectedNotes = "Some other notes"
         adapter.notes = expectedNotes
         
-        XCTAssertEqual(expectedNotes, adapter.notes)
+        XCTAssertEqual(expectedNotes, event.notes)
+    }
+    
+    func testAccessingTheEventLocationShouldReturnTheLocationFromTheUnderlyingEvent() {
+        XCTAssertEqual(event.location, adapter.location)
+    }
+    
+    func testSettingTheEventLocationShouldUpdateTheLocationOnTheUnderlyingEvent() {
+        let expectedLocation = "Some other location"
+        adapter.location = expectedLocation
+        
+        XCTAssertEqual(expectedLocation, event.location)
+    }
+    
+    func testAccessingTheEventStartDateShouldReturnTheStartDateFromTheUnderlyingEvent() {
+        XCTAssertEqual(event.startDate, adapter.startDate)
+    }
+    
+    func testSettingTheEventStartDateShouldUpdateTheStartDateOnTheUnderlyingEvent() {
+        let expectedStartDate = Date(timeIntervalSinceReferenceDate: 1000)
+        adapter.startDate = expectedStartDate
+        
+        XCTAssertEqual(expectedStartDate, event.startDate)
+    }
+    
+    func testAccessingTheEventEndDateShouldReturnTheEndDateFromTheUnderlyingEvent() {
+        XCTAssertEqual(event.endDate, adapter.endDate)
+    }
+    
+    func testSettingTheEventEndDateShouldUpdateTheEndDateOnTheUnderlyingEvent() {
+        let expectedEndDate = Date(timeIntervalSinceReferenceDate: 1000)
+        adapter.endDate = expectedEndDate
+        
+        XCTAssertEqual(expectedEndDate, event.endDate)
+    }
+    
+    func testAddingAnAlarmShouldAddTheAlarmToTheUnderlyingEvent() {
+        let relativeOffset: TimeInterval = 100
+        adapter.addAlarm(relativeOffsetFromStartDate: relativeOffset)
+        
+        XCTAssertEqual(relativeOffset, event.alarms?.first?.relativeOffset)
     }
     
 }
