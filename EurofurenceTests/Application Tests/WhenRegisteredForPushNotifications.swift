93a13cd73dea963543e31185ebb65a417ddc7623
ShezHsky <shezhsky@icloud.com>
2017-07-14 22:07:39 +0100

    Ensure notification type registration waits for token

--- EurofurenceTests/Application Tests/WhenRegisteredForPushNotifications.swift
+++ EurofurenceTests/Application Tests/WhenRegisteredForPushNotifications.swift
@@ -62,21 +62,24 @@ class CapturingNotificationsService: NotificationsService {
 
 struct EurofurenceApplication {
 
+    private var buildConfiguration: BuildConfigurationProviding
+    private var notificationsService: NotificationsService
+
     init(buildConfiguration: BuildConfigurationProviding,
          notificationsService: NotificationsService) {
+        self.buildConfiguration = buildConfiguration
+        self.notificationsService = notificationsService
+    }
+
+    func handleRemoteNotificationRegistration(deviceToken: Data) {
         switch buildConfiguration.configuration {
         case .debug:
             notificationsService.subscribeToTestNotifications()
             notificationsService.unsubscribeFromLiveNotifications()
-
         case .release:
             notificationsService.subscribeToLiveNotifications()
             notificationsService.unsubscribeFromTestNotifications()
         }
-    }
-
-    func handleRemoteNotificationRegistration(deviceToken: Data) {
-
     }
 
 }
@@ -156,6 +159,26 @@ class WhenRegisteredForPushNotifications: XCTestCase {
         context.registerForNotifications()
 
         XCTAssertFalse(context.capturingNotificationService.unsubscribedFromLiveNotifications)
+    }
+
+    func testForDebugConfigurationNotRegisterForTestNotificationsUntilWeActuallyReceievePushToken() {
+        let context = assembleApp(configuration: .debug)
+        XCTAssertFalse(context.capturingNotificationService.subscribedToTestNotifications)
+    }
+
+    func testForReleaseConfigurationNotRegisterForLiveNotificationsUntilWeActuallyReceievePushToken() {
+        let context = assembleApp(configuration: .release)
+        XCTAssertFalse(context.capturingNotificationService.subscribedToLiveNotifications)
+    }
+
+    func testForDebugConfigurationNotUnregisterFromLiveNotificationsUntilWeActuallyReceievePushToken() {
+        let context = assembleApp(configuration: .debug)
+        XCTAssertFalse(context.capturingNotificationService.unsubscribedFromLiveNotifications)
+    }
+
+    func testForReleaseConfigurationNotUnregisterFromTestNotificationsUntilWeActuallyReceievePushToken() {
+        let context = assembleApp(configuration: .release)
+        XCTAssertFalse(context.capturingNotificationService.unsubscribedFromTestNotifications)
     }
     
 }
