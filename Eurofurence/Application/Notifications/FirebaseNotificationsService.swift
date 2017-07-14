8f663d1c0fe7acaf9deb8a7fbed296a0ccb9a6e7
ShezHsky <shezhsky@icloud.com>
2017-07-14 22:24:31 +0100

    Add thin adapters for build config and notifications

---
+++ Eurofurence/Application/Notifications/FirebaseNotificationsService.swift
@@ -0,0 +1,36 @@
+//
+//  FirebaseNotificationsService.swift
+//  Eurofurence
+//
+//  Created by Thomas Sherwood on 14/07/2017.
+//  Copyright © 2017 Eurofurence. All rights reserved.
+//
+
+import FirebaseMessaging
+import Foundation
+
+struct FirebaseNotificationsService: NotificationsService {
+
+    private let messaging = Messaging.messaging()
+
+    func register(deviceToken: Data) {
+        messaging.setAPNSToken(deviceToken, type: .unknown)
+    }
+
+    func subscribeToTestNotifications() {
+        messaging.subscribe(toTopic: "test")
+    }
+
+    func unsubscribeFromTestNotifications() {
+        messaging.unsubscribe(fromTopic: "test")
+    }
+
+    func subscribeToLiveNotifications() {
+        messaging.subscribe(toTopic: "live")
+    }
+
+    func unsubscribeFromLiveNotifications() {
+        messaging.unsubscribe(fromTopic: "live")
+    }
+    
+}
