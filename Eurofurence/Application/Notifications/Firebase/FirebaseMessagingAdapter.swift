956b7b29312dc0441de4af7d593b0f5d1daf8d3e
ShezHsky <shezhsky@icloud.com>
2017-07-15 10:47:12 +0100

    Add concrete firebase adapter

---
+++ Eurofurence/Application/Notifications/Firebase/FirebaseMessagingAdapter.swift
@@ -0,0 +1,32 @@
+//
+//  FirebaseMessagingAdapter.swift
+//  Eurofurence
+//
+//  Created by Thomas Sherwood on 15/07/2017.
+//  Copyright © 2017 Eurofurence. All rights reserved.
+//
+
+import FirebaseMessaging
+import Foundation
+
+struct FirebaseMessagingAdapter: FirebaseAdapter {
+
+    private let messaging = Messaging.messaging()
+
+    var fcmToken: String {
+        return messaging.fcmToken ?? ""
+    }
+
+    func setAPNSToken(deviceToken: Data) {
+        messaging.setAPNSToken(deviceToken, type: .unknown)
+    }
+
+    func subscribe(toTopic topic: FirebaseTopic) {
+        messaging.subscribe(toTopic: topic.rawValue)
+    }
+
+    func unsubscribe(fromTopic topic: FirebaseTopic) {
+        messaging.unsubscribe(fromTopic: topic.rawValue)
+    }
+
+}
