4642fc4a7326b726b797e26d28a282f4a64b2c24
ShezHsky <shezhsky@icloud.com>
2017-07-25 22:46:10 +0100

    Remove duplication in JSONSession

--- Eurofurence/Services/HTTP/URLSessionBasedJSONSession.swift
+++ Eurofurence/Services/HTTP/URLSessionBasedJSONSession.swift
@@ -13,25 +13,19 @@ struct URLSessionBasedJSONSession: JSONSession {
     var session: URLSession = .shared
 
     func get(_ request: Request, completionHandler: @escaping (Data?) -> Void) {
-        guard let actualURL = URL(string: request.url) else { return }
-
-        var urlRequest = URLRequest(url: actualURL)
-        urlRequest.setValue("application/json", forHTTPHeaderField: "Content-Type")
-        urlRequest.allHTTPHeaderFields = request.headers
-
-        session.dataTask(with: urlRequest, completionHandler: { (data, _, _) in
-            DispatchQueue.main.async {
-                completionHandler(data)
-            }
-        }).resume()
+        perform(request, method: "GET", completionHandler: completionHandler)
     }
 
     func post(_ request: Request, completionHandler: @escaping (Data?) -> Void) {
+        perform(request, method: "POST", completionHandler: completionHandler)
+    }
+
+    private func perform(_ request: Request, method: String, completionHandler: @escaping (Data?) -> Void) {
         guard let actualURL = URL(string: request.url) else { return }
 
         var urlRequest = URLRequest(url: actualURL)
         urlRequest.setValue("application/json", forHTTPHeaderField: "Content-Type")
-        urlRequest.httpMethod = "POST"
+        urlRequest.httpMethod = method
         urlRequest.httpBody = request.body
         urlRequest.allHTTPHeaderFields = request.headers
 
