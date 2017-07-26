8e70287ab49ef1c2bb51ffbe58bc4a0a82b65c38
Fenrikur <ef-app-ios@fenrikur.de>
2017-07-26 17:08:20 +0200

    Protocol for routing to a target view controller on a specified navigation controller providing an optional payload

---
+++ Eurofurence/Views/Routers/TargetRouter.swift
@@ -0,0 +1,27 @@
+//
+//  TargetRouter.swift
+//  Eurofurence
+//
+//  Copyright © 2017 Eurofurence. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+protocol TargetRouter {
+	func show(target: RoutingTarget)
+}
+
+struct RoutingTarget {
+	var viewControllerIdentifier: String
+	var navigationControllerIdentifier: String
+	var payload: [String : Any]?
+
+	init(target viewControllerIdentifier: String,
+	                 on navigationControllerIdentifier: String,
+	                 payload: [String : Any]? = nil) {
+		self.viewControllerIdentifier = viewControllerIdentifier
+		self.navigationControllerIdentifier = navigationControllerIdentifier
+		self.payload = payload
+	}
+}
