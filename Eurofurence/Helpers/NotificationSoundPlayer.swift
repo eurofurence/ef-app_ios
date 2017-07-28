eb0383f694ebaeb9ddab24ba3f4fa0a64d08e084
Fenrikur <ef-app-ios@fenrikur.de>
2017-07-29 00:26:43 +0200

    Add function to copy sound files from bundle to Library/Sounds in order to allow static sound names on remote notifications [#103]

--- Eurofurence/Helpers/NotificationSoundPlayer.swift
+++ Eurofurence/Helpers/NotificationSoundPlayer.swift
@@ -9,8 +9,17 @@ import AudioToolbox
 
 class NotificationSoundPlayer {
 	static let shared = NotificationSoundPlayer()
+	private static let soundsLibraryDirectoryName = "Sounds"
 
+	private let baseDirectory = FileManager.default.urls(for: .libraryDirectory, in: .userDomainMask)[0]
+	private var soundsLibraryDirectory: URL
 	private var soundIds: [String : SystemSoundID] = [:]
+
+	init() {
+		soundsLibraryDirectory = baseDirectory.appendingPathComponent(
+			NotificationSoundPlayer.soundsLibraryDirectoryName,
+			isDirectory: true)
+	}
 
 	func playSound(for soundName: String?) {
 		guard let soundId = getSystemSoundId(for: soundName) else {
@@ -36,6 +45,27 @@ class NotificationSoundPlayer {
 		soundIds[soundName] = soundId
 
 		return soundId
+	}
+
+	func copyToSoundsLibrary(_ soundName: String, targetName: String) throws {
+		try checkSoundsLibraryDirectory()
+		guard let soundUrl = Bundle.main.url(forResource: soundName, withExtension: nil)
+			else { return }
+		let targetUrl = soundsLibraryDirectory.appendingPathComponent(targetName,
+		                                                             isDirectory: false)
+
+		if FileManager.default.fileExists(atPath: targetUrl.path) {
+			try FileManager.default.removeItem(at: targetUrl)
+		}
+
+		try FileManager.default.copyItem(at: soundUrl, to: targetUrl)
+	}
+
+	private func checkSoundsLibraryDirectory() throws {
+		if !FileManager.default.fileExists(atPath: soundsLibraryDirectory.path) {
+			try FileManager.default.createDirectory(at: soundsLibraryDirectory, withIntermediateDirectories: true)
+			try soundsLibraryDirectory.setExcludedFromBackup(true)
+		}
 	}
 }
 
