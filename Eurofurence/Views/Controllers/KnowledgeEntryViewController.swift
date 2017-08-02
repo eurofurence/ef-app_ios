b7a4ea259965453ac2a51076db54ddd4837240bd
Fenrikur <ef-app-ios@fenrikur.de>
2017-08-02 15:17:37 +0200

    Change knowledge detail view to be based on vertical stack view [#109]

--- Eurofurence/Views/Controllers/KnowledgeEntryViewController.swift
+++ Eurofurence/Views/Controllers/KnowledgeEntryViewController.swift
@@ -11,18 +11,14 @@ import ReactiveSwift
 class KnowledgeEntryViewController: UIViewController {
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var imageView: UIImageView!
-    @IBOutlet weak var imageViewHeightConstraint: NSLayoutConstraint!
     @IBOutlet weak var textView: UITextView!
     @IBOutlet weak var linkLabel: UILabel!
-    @IBOutlet weak var linkView: UIView!
+    @IBOutlet weak var linkView: UIStackView!
 
 	let imageService: ImageServiceProtocol = try! ServiceResolver.container.resolve()
 	weak var knowledgeEntry = KnowledgeEntry()
 	var disposables = CompositeDisposable()
 
-    var imageViewDefaultHeight = CGFloat(0.0)
-    var linkViewLastButton: UIButton?
-    var linkViewLastBottomConstraint: NSLayoutConstraint?
 	// TODO: This is somewhat nasty and might require refactoring at some point
     var buttonLinks: [UIButton:LinkFragment] = [:]
 
@@ -52,47 +48,40 @@ class KnowledgeEntryViewController: UIViewController {
 		navigationItem.title = knowledgeEntry?.KnowledgeGroup?.Name
         titleLabel.text = knowledgeEntry?.Title
 
+		imageView.image = #imageLiteral(resourceName: "ef")
         // TODO: How should multiple images be handled?
         if let image = knowledgeEntry?.Images?.first {
+			imageView.isHidden = false
 			imageService.retrieve(for: image).startWithResult({ [unowned self] result in
 				switch result {
 				case let .success(uiImage):
 					DispatchQueue.main.async {
 						self.imageView.image = uiImage
-						if self.imageViewDefaultHeight > 0 {
-							self.imageViewHeightConstraint.constant = self.imageViewDefaultHeight
-						}
 					}
 				case let .failure(error):
 					print("Failed to retrieve image for KnowledgeEntry \(self.knowledgeEntry?.Title ?? ""): \(error)")
 				}
 			})
-        }
-
-		imageView.image = nil
-		if imageViewDefaultHeight == 0 {
-			imageViewDefaultHeight = imageViewHeightConstraint.constant
+		} else {
+			imageView.isHidden = true
 		}
-		imageViewHeightConstraint.constant = 0.0
-        imageView.sizeToFit()
 
 		textView.attributedText = WikiText.transform(knowledgeEntry?.Text ?? "")
 
         for subview in linkView.subviews {
+			linkView.removeArrangedSubview(subview)
             subview.removeFromSuperview()
         }
-        linkViewLastButton = nil
-        linkViewLastBottomConstraint = nil
         buttonLinks = [:]
 
-        linkView.translatesAutoresizingMaskIntoConstraints = false
 		if let links = knowledgeEntry?.Links {
 			for knowledgeLink in links {
 				addLinkButton(knowledgeLink)
 			}
 		}
 
-        linkLabel.isHidden = buttonLinks.count == 0
+		linkLabel.isHidden = buttonLinks.count == 0
+		linkView.isHidden = buttonLinks.count == 0
     }
 
 	override func viewWillDisappear(_ animated: Bool) {
@@ -126,35 +115,13 @@ class KnowledgeEntryViewController: UIViewController {
     func addLinkButton(_ linkFragment: LinkFragment) {
         let linkButton = UIButton(type: UIButtonType.roundedRect)
         linkButton.setTitle(linkFragment.Name, for: UIControlState())
+		linkButton.titleLabel?.font = UIFont.preferredFont(forTextStyle: .body)
         linkButton.accessibilityIdentifier = linkFragment.Name
-        linkButton.translatesAutoresizingMaskIntoConstraints = false
         linkButton.addTarget(self, action: #selector(KnowledgeEntryViewController.urlButtonAction(_:)), for: .touchUpInside)
 
+		linkView.addArrangedSubview(linkButton)
         linkView.addSubview(linkButton)
-
-        if linkViewLastButton != nil {
-            if linkViewLastBottomConstraint != nil {
-                linkView.removeConstraint(linkViewLastBottomConstraint!)
-            }
-            // Top Constraint -> lastButton.Bottom
-            NSLayoutConstraint(item: linkButton, attribute: NSLayoutAttribute.top, relatedBy: NSLayoutRelation.equal, toItem: linkViewLastButton, attribute: NSLayoutAttribute.bottom, multiplier: 1.0, constant: 4).isActive = true
-        } else {
-            // Top Constraint -> view.TopMargin
-            NSLayoutConstraint(item: linkButton, attribute: NSLayoutAttribute.top, relatedBy: NSLayoutRelation.equal, toItem: linkView, attribute: NSLayoutAttribute.topMargin, multiplier: 1.0, constant: 0).isActive = true
-        }
-        // Bottom Constraint -> view.Bottom
-        linkViewLastBottomConstraint = NSLayoutConstraint(item: linkButton, attribute: NSLayoutAttribute.bottom, relatedBy: NSLayoutRelation.equal, toItem: linkView, attribute: NSLayoutAttribute.bottomMargin, multiplier: 1.0, constant: 0)
-        linkViewLastBottomConstraint!.isActive = true
-        // Leading Constraint -> view.LeadingMargin
-        NSLayoutConstraint(item: linkButton, attribute: NSLayoutAttribute.leading, relatedBy: NSLayoutRelation.equal, toItem: linkView, attribute: NSLayoutAttribute.leadingMargin, multiplier: 1.0, constant: 0).isActive = true
-        // Trailing Constraint -> view.TrailingMargin
-        NSLayoutConstraint(item: linkButton, attribute: NSLayoutAttribute.trailing, relatedBy: NSLayoutRelation.equal, toItem: linkView, attribute: NSLayoutAttribute.trailingMargin, multiplier: 1.0, constant: 0).isActive = true
-
-        linkViewLastButton = linkButton
         buttonLinks[linkButton] = linkFragment
-
-        linkView.layoutSubviews()
-
     }
 
     override func didReceiveMemoryWarning() {
