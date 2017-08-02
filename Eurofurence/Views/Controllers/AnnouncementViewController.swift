7837ffb5379bb25c9b460b3b2a65826f77e98f98
Fenrikur <ef-app-ios@fenrikur.de>
2017-08-02 22:30:33 +0200

    Added tiled background to announcement view [#109]

--- Eurofurence/Views/Controllers/AnnouncementViewController.swift
+++ Eurofurence/Views/Controllers/AnnouncementViewController.swift
@@ -20,7 +20,8 @@ class AnnouncementViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        view.backgroundColor = UIColor(patternImage: #imageLiteral(resourceName: "Background Tile"))
+		descriptionLabel.textContainer.lineFragmentPadding = 0.0
     }
 
     func canRotate() -> Bool {
