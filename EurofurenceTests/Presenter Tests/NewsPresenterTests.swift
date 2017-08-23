c142c3998e98abf7d92078520817bf7f87af0927
ShezHsky <shezhsky@icloud.com>
2017-08-23 21:53:22 +0100

    Show messages navigation options on news presenter when logged in

--- EurofurenceTests/Presenter Tests/NewsPresenterTests.swift
+++ EurofurenceTests/Presenter Tests/NewsPresenterTests.swift
@@ -9,25 +9,59 @@
 @testable import Eurofurence
 import XCTest
 
+enum AuthState {
+    case loggedIn(User)
+    case loggedOut
+}
+
 protocol AuthService {
     
-    func determineAuthState()
+    func determineAuthState(completionHandler: @escaping (AuthState) -> Void)
+    
+}
+
+protocol NewsScene {
+    
+    func showMessagesNavigationAction()
     
 }
 
 class CapturingAuthService: AuthService {
     
+    private let loggedInUser: User?
+    
+    init(loggedInUser: User? = nil) {
+        self.loggedInUser = loggedInUser
+    }
+    
     private(set) var wasRequestedToDetermineAuthState = false
-    func determineAuthState() {
+    func determineAuthState(completionHandler: @escaping (AuthState) -> Void) {
         wasRequestedToDetermineAuthState = true
+        
+        if let user = loggedInUser {
+            completionHandler(.loggedIn(user))
+        }
+        else {
+            completionHandler(.loggedOut)
+        }
+    }
+    
+}
+
+class CapturingNewsScene: NewsScene {
+    
+    private(set) var wasToldToShowMessagesNavigationAction = false
+    func showMessagesNavigationAction() {
+        wasToldToShowMessagesNavigationAction = true
     }
     
 }
 
 struct NewsPresenter {
     
-    init(authService: AuthService) {
-        authService.determineAuthState()
+    init(authService: AuthService, newsScene: NewsScene) {
+        authService.determineAuthState() { _ in }
+        newsScene.showMessagesNavigationAction()
     }
     
 }
@@ -36,9 +70,18 @@ class NewsPresenterTests: XCTestCase {
     
     func testWhenLaunchedTheAuthServiceIsRequestedTheLoginState() {
         let authService = CapturingAuthService()
-        _ = NewsPresenter(authService: authService)
+        _ = NewsPresenter(authService: authService, newsScene: CapturingNewsScene())
         
         XCTAssertTrue(authService.wasRequestedToDetermineAuthState)
+    }
+    
+    func testWhenLaunchedWithLoggedInUserTheSceneIsToldToShowTheMessagesNavigationAction() {
+        let user = User(registrationNumber: 42, username: "")
+        let authService = CapturingAuthService(loggedInUser: user)
+        let newsScene = CapturingNewsScene()
+        _ = NewsPresenter(authService: authService, newsScene: newsScene)
+        
+        XCTAssertTrue(newsScene.wasToldToShowMessagesNavigationAction)
     }
     
 }
