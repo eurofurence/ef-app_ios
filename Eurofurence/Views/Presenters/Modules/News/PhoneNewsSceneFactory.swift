73191d91e4df9dc38ce467080f2054047b064531
ShezHsky <shezhsky@icloud.com>
2017-11-06 12:57:30 +0000

    Add news scene factory

---
+++ Eurofurence/Views/Presenters/Modules/News/PhoneNewsSceneFactory.swift
@@ -0,0 +1,18 @@
+//
+//  PhoneNewsSceneFactory.swift
+//  Eurofurence
+//
+//  Created by Thomas Sherwood on 06/11/2017.
+//  Copyright © 2017 Eurofurence. All rights reserved.
+//
+
+import UIKit.UIStoryboard
+
+struct PhoneNewsSceneFactory: NewsSceneFactory {
+    
+    func makeNewsScene() -> UIViewController & NewsScene {
+        let storyboard = UIStoryboard(name: "Main", bundle: .main)
+        return storyboard.instantiateViewController(withIdentifier: "NewsTableView") as! NewsTableViewController
+    }
+    
+}
