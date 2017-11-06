1829827571d173db885d2509f5bd384ad1d8af3c
ShezHsky <shezhsky@icloud.com>
2017-11-06 12:54:37 +0000

    Added tutorial scene factory implementation

---
+++ Eurofurence/Views/Presenters/Modules/Tutorial/PhoneTutorialSceneFactory.swift
@@ -0,0 +1,18 @@
+//
+//  PhoneTutorialSceneFactory.swift
+//  Eurofurence
+//
+//  Created by Thomas Sherwood on 06/11/2017.
+//  Copyright © 2017 Eurofurence. All rights reserved.
+//
+
+import UIKit.UIStoryboard
+
+struct PhoneTutorialSceneFactory: TutorialSceneFactory {
+    
+    func makeTutorialScene() -> UIViewController & TutorialScene {
+        let storyboard = UIStoryboard(name: "Tutorial", bundle: .main)
+        return storyboard.instantiateInitialViewController() as! TutorialViewController
+    }
+    
+}
