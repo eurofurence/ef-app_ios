043e182076c513714cbb8a06099de4d34f66f0b2
ShezHsky <shezhsky@icloud.com>
2017-11-06 12:58:27 +0000

    Add navigation controller factory

---
+++ Eurofurence/Views/Presenters/Director/PhoneNavigationControllerFactory.swift
@@ -0,0 +1,17 @@
+//
+//  PhoneNavigationControllerFactory.swift
+//  Eurofurence
+//
+//  Created by Thomas Sherwood on 06/11/2017.
+//  Copyright © 2017 Eurofurence. All rights reserved.
+//
+
+import UIKit.UINavigationController
+
+struct PhoneNavigationControllerFactory: NavigationControllerFactory {
+    
+    func makeNavigationController() -> UINavigationController {
+        return UINavigationController()
+    }
+    
+}
