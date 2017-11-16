917627c8147f8a94090c60bb16d95b2a77595cd8
ShezHsky <shezhsky@icloud.com>
2017-11-16 13:05:55 +0000

    Refactor login presenter

--- Eurofurence/Views/Presenters/Modules/Login/LoginPresenter.swift
+++ Eurofurence/Views/Presenters/Modules/Login/LoginPresenter.swift
@@ -8,27 +8,50 @@
 
 import Foundation
 
-struct LoginPresenter: LoginSceneDelegate {
+class LoginPresenter: LoginSceneDelegate {
 
     private let delegate: LoginModuleDelegate
     private let scene: LoginScene
-    private let inputContainer = InputContainer()
+    private lazy var validator = LoginValidator(validationHandler: self.loginValidationStateDidChange)
+    private lazy var validationActions: [LoginValidator.Result : () -> Void] = [
+        .valid: self.scene.enableLoginButton,
+        .invalid: self.scene.disableLoginButton
+    ]
 
-    private class InputContainer {
-        var registrationNumber: Int?
-        var username: String?
-        var password: String?
-
-        var isValid: Bool {
-            guard let username = username, let password = password else { return false }
-            return registrationNumber != nil && !username.isEmpty && !password.isEmpty
+    private class LoginValidator {
+        enum Result {
+            case valid
+            case invalid
         }
 
-        func updateRegistrationNumber(with string: String) {
-            var container = 0
-            if Scanner(string: string).scanInt(&container) {
-                registrationNumber = container
-            }
+        var registrationNumber: String? {
+            didSet { validate() }
+        }
+
+        var username: String? {
+            didSet { validate() }
+        }
+
+        var password: String? {
+            didSet { validate() }
+        }
+
+        private let  validationHandler: (Result) -> Void
+
+        init(validationHandler: @escaping (Result) -> Void) {
+            self.validationHandler = validationHandler
+        }
+
+        private var isValid: Bool {
+            guard let registrationNumber = registrationNumber,
+                  let username = username,
+                  let password = password else { return false }
+
+            return Scanner(string: registrationNumber).scanInt(nil) && !username.isEmpty && !password.isEmpty
+        }
+
+        private func validate() {
+            validationHandler(isValid ? .valid : .invalid)
         }
     }
 
@@ -45,26 +68,19 @@ struct LoginPresenter: LoginSceneDelegate {
     }
 
     func loginSceneDidUpdateRegistrationNumber(_ registrationNumberString: String) {
-        inputContainer.updateRegistrationNumber(with: registrationNumberString)
-        updateLoginButtonInteractivity()
+        validator.registrationNumber = registrationNumberString
     }
 
     func loginSceneDidUpdateUsername(_ username: String) {
-        inputContainer.username = username
-        updateLoginButtonInteractivity()
+        validator.username = username
     }
 
     func loginSceneDidUpdatePassword(_ password: String) {
-        inputContainer.password = password
-        updateLoginButtonInteractivity()
+        validator.password = password
     }
 
-    private func updateLoginButtonInteractivity() {
-        if inputContainer.isValid {
-            scene.enableLoginButton()
-        } else {
-            scene.disableLoginButton()
-        }
+    private func loginValidationStateDidChange(_ state: LoginValidator.Result) {
+        validationActions[state]?()
     }
 
 }
