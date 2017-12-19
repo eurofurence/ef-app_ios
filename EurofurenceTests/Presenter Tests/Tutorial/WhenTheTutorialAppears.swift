53bcf7f463a44fb293c0a87def218514a6e9563d
ShezHsky <shezhsky@icloud.com>
2017-12-19 20:09:29 +0000

    Rework initial tutorial presentation tests to use context

---
+++ EurofurenceTests/Presenter Tests/Tutorial/WhenTheTutorialAppears.swift
@@ -0,0 +1,29 @@
+//
+//  WhenTheTutorialAppears.swift
+//  EurofurenceTests
+//
+//  Created by Thomas Sherwood on 19/12/2017.
+//  Copyright © 2017 Eurofurence. All rights reserved.
+//
+
+@testable import Eurofurence
+import XCTest
+
+class WhenTheTutorialAppears: XCTestCase {
+    
+    var context: TutorialModuleTestBuilder.Context!
+    
+    override func setUp() {
+        super.setUp()
+        context = TutorialModuleTestBuilder().build()
+    }
+    
+    func testItShouldBeToldToShowTheTutorialPage() {
+        XCTAssertTrue(context.tutorial.wasToldToShowTutorialPage)
+    }
+    
+    func testItShouldReturnTheViewControllerFromTheFactory() {
+        XCTAssertEqual(context.tutorialViewController, context.tutorial)
+    }
+    
+}
