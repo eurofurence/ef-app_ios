84ba64c60e429f1ff3110cf36063f5354736cfc0
ShezHsky <shezhsky@icloud.com>
2018-04-07 12:43:07 +0100

    Delete unused text context

--- EurofurenceTests/Presenter Tests/News/NewsPresenterTestContext.swift
+++ EurofurenceTests/Presenter Tests/News/NewsPresenterTestContext.swift
@@ -72,39 +72,3 @@ extension NewsPresenterTestBuilder.Context {
     
 }
 
-struct NewsPresenterTestContext {
-    
-    let authService: StubAuthenticationService
-    let privateMessagesService: CapturingPrivateMessagesService
-    let sceneFactory = StubNewsSceneFactory()
-    let delegate = CapturingNewsModuleDelegate()
-    
-    var newsScene: CapturingNewsScene { return sceneFactory.stubbedScene }
-    
-    @discardableResult
-    static func makeTestCaseForAuthenticatedUser(_ user: User = User(registrationNumber: 42, username: ""),
-                                                 privateMessagesService: CapturingPrivateMessagesService = CapturingPrivateMessagesService()) -> NewsPresenterTestContext {
-        return NewsPresenterTestContext(authenticationService: StubAuthenticationService(authState: .loggedIn(user)),
-                                      privateMessagesService: privateMessagesService)
-    }
-    
-    @discardableResult
-    static func makeTestCaseForAnonymousUser() -> NewsPresenterTestContext {
-        return NewsPresenterTestContext(authenticationService: StubAuthenticationService(authState: .loggedOut),
-                                      privateMessagesService: CapturingPrivateMessagesService())
-    }
-    
-    private init(authenticationService: StubAuthenticationService,
-                 privateMessagesService: CapturingPrivateMessagesService) {
-        self.authService = authenticationService
-        self.privateMessagesService = privateMessagesService
-        _ = NewsModuleBuilder()
-            .with(sceneFactory)
-            .with(authenticationService)
-            .with(privateMessagesService)
-            .build()
-            .makeNewsModule(delegate)
-        sceneFactory.stubbedScene.delegate?.newsSceneWillAppear()
-    }
-    
-}
