2be8233e33978d7f5554bbdadd45a2a517af28fa
ShezHsky <shezhsky@icloud.com>
2018-04-19 19:53:45 +0100

    Add event and announcement component scenes to Storyboard

---
+++ Eurofurence/Modules/News/View/NewsAnnouncementTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  NewsEventTableViewCell.swift
+//  Eurofurence
+//
+//  Created by Thomas Sherwood on 19/04/2018.
+//  Copyright © 2018 Eurofurence. All rights reserved.
+//
+
+import UIKit
+
+class NewsAnnouncementTableViewCell: UITableViewCell {
+
+    // MARK: IBOutlets
+
+    @IBOutlet weak var announcementTitleLabel: UILabel!
+    @IBOutlet weak var announcementDescriptionLabel: UILabel!
+
+}
---
+++ Eurofurence/Modules/News/View/NewsEventTableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  NewsEventTableViewCell.swift
+//  Eurofurence
+//
+//  Created by Thomas Sherwood on 19/04/2018.
+//  Copyright © 2018 Eurofurence. All rights reserved.
+//
+
+import UIKit
+
+class NewsEventTableViewCell: UITableViewCell {
+
+    // MARK: IBOutlets
+
+    @IBOutlet weak var eventStartTimeLabel: UILabel!
+    @IBOutlet weak var eventEndTimeLabel: UILabel!
+    @IBOutlet weak var eventIconImageView: UIImageView!
+    @IBOutlet weak var eventNameLabel: UILabel!
+    @IBOutlet weak var eventLocationLabel: UILabel!
+
+}
