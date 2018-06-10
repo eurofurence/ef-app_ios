ac74607bad50f73250b8ed37701fd9a134ba9003
ShezHsky <shezhsky@icloud.com>
2018-06-10 22:25:29 +0100

    Event detail layout spruce-up

--- Eurofurence/Modules/Event Detail/View/UIKit/EventDetailBannerTableViewCell.swift
+++ Eurofurence/Modules/Event Detail/View/UIKit/EventDetailBannerTableViewCell.swift
@@ -17,7 +17,31 @@ class EventDetailBannerTableViewCell: UITableViewCell, EventGraphicComponent {
     // MARK: EventGraphicComponent
 
     func setPNGGraphicData(_ pngGraphicData: Data) {
-        bannerImageView.image = UIImage(data: pngGraphicData)
+        let image = UIImage(data: pngGraphicData)
+        bannerImageView.image = image
+
+        if let image = image {
+            applyAspectRatioConstraint(image.size)
+        }
+    }
+
+    // MARK: Framing Workaround
+
+    private var aspectRatioConstraint: NSLayoutConstraint?
+    private func applyAspectRatioConstraint(_ size: CGSize) {
+        if let aspectRatioConstraint = aspectRatioConstraint {
+            bannerImageView.removeConstraint(aspectRatioConstraint)
+        }
+
+        let constraint = NSLayoutConstraint(item: bannerImageView,
+                                            attribute: .height,
+                                            relatedBy: .equal,
+                                            toItem: bannerImageView,
+                                            attribute: .width,
+                                            multiplier: size.height / size.width,
+                                            constant: 0)
+        bannerImageView.addConstraint(constraint)
+        aspectRatioConstraint = constraint
     }
 
 }
