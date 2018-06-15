62553a4fa6d8ddab8c1e0d0114b09fa1ae76a550
ShezHsky <shezhsky@icloud.com>
2018-06-15 23:32:14 +0100

    Shift test double into its own file

---
+++ EurofurenceTests/Presenter Tests/Event Detail/Test Doubles/FakeEventsService.swift
@@ -0,0 +1,95 @@
+//
+//  FakeEventsService.swift
+//  EurofurenceTests
+//
+//  Created by Thomas Sherwood on 15/06/2018.
+//  Copyright © 2018 Eurofurence. All rights reserved.
+//
+
+@testable import Eurofurence
+import Foundation
+
+class FakeEventsService: EventsService {
+    
+    var runningEvents: [Event2] = []
+    var upcomingEvents: [Event2] = []
+    var allEvents: [Event2] = []
+    var favourites: [Event2.Identifier] = []
+    var allDays: [Day] = []
+    var currentDay: Day?
+    
+    init(favourites: [Event2.Identifier] = []) {
+        self.favourites = favourites
+    }
+    
+    private var observers = [EventsServiceObserver]()
+    func add(_ observer: EventsServiceObserver) {
+        observers.append(observer)
+        
+        observer.eventsDidChange(to: allEvents)
+        observer.runningEventsDidChange(to: runningEvents)
+        observer.upcomingEventsDidChange(to: upcomingEvents)
+        observer.favouriteEventsDidChange(favourites)
+        observer.eventDaysDidChange(to: allDays)
+        observer.currentEventDayDidChange(to: currentDay)
+    }
+    
+    private(set) var favouritedEventIdentifier: Event2.Identifier?
+    func favouriteEvent(identifier: Event2.Identifier) {
+        favouritedEventIdentifier = identifier
+        favourites.append(identifier)
+        observers.forEach { $0.favouriteEventsDidChange(favourites) }
+    }
+    
+    private(set) var unfavouritedEventIdentifier: Event2.Identifier?
+    func unfavouriteEvent(identifier: Event2.Identifier) {
+        unfavouritedEventIdentifier = identifier
+        if let idx = favourites.index(of: identifier) {
+            favourites.remove(at: idx)
+        }
+        
+        observers.forEach { $0.favouriteEventsDidChange([]) }
+    }
+    
+    private(set) var lastProducedSchedule: FakeEventsSchedule?
+    func makeEventsSchedule() -> EventsSchedule {
+        let schedule = FakeEventsSchedule()
+        lastProducedSchedule = schedule
+        return schedule
+    }
+    
+}
+
+extension FakeEventsService {
+    
+    func stubSomeFavouriteEvents() {
+        allEvents = .random(minimum: 3)
+        favourites = Array(allEvents.dropFirst()).map({ $0.identifier })
+    }
+    
+    func simulateEventFavourited(identifier: Event2.Identifier) {
+        favourites.append(identifier)
+        observers.forEach { $0.favouriteEventsDidChange(favourites) }
+    }
+    
+    func simulateEventUnfavourited(identifier: Event2.Identifier) {
+        if let idx = favourites.index(of: identifier) {
+            favourites.remove(at: idx)
+        }
+        
+        observers.forEach { $0.favouriteEventsDidChange(favourites) }
+    }
+    
+    func simulateEventsChanged(_ events: [Event2]) {
+        observers.forEach { $0.eventsDidChange(to: events) }
+    }
+    
+    func simulateDaysChanged(_ days: [Day]) {
+        observers.forEach { $0.eventDaysDidChange(to: days) }
+    }
+    
+    func simulateDayChanged(to day: Day) {
+        observers.forEach { $0.currentEventDayDidChange(to: day) }
+    }
+    
+}
