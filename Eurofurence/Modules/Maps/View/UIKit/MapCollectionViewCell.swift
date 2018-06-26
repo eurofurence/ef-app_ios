36d59ff8b844b7adce04caf149a7b9fbfd71335b
ShezHsky <shezhsky@icloud.com>
2018-06-26 20:03:34 +0100

    Stub out map cell to infer required information

---
+++ Eurofurence/Modules/Maps/View/UIKit/MapCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  MapCollectionViewCell.swift
+//  Eurofurence
+//
+//  Created by Thomas Sherwood on 26/06/2018.
+//  Copyright © 2018 Eurofurence. All rights reserved.
+//
+
+import UIKit
+
+class MapCollectionViewCell: UICollectionViewCell {
+    
+    // MARK: Properties
+    
+    @IBOutlet weak var mapPreviewImageView: UIImageView!
+    @IBOutlet weak var mapNameLabel: UILabel!
+    
+}
