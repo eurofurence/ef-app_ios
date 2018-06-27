2bb35f86a3cc500da4f4fc40f708e74f7f867901
ShezHsky <shezhsky@icloud.com>
2018-06-27 21:16:51 +0100

    Adapt map data into view model

--- Eurofurence/Modules/Map Detail/Interactor/DefaultMapDetailInteractor.swift
+++ Eurofurence/Modules/Map Detail/Interactor/DefaultMapDetailInteractor.swift
@@ -9,30 +9,35 @@
 import Foundation
 
 class DefaultMapDetailInteractor: MapDetailInteractor, MapsObserver {
-    
+
     private struct ViewModel: MapDetailViewModel {
-        
+
         var mapImagePNGData: Data = Data()
         var mapName: String = ""
-        
+
     }
-    
+
+    private let mapsService: MapsService
     private var maps = [Map2]()
-    
+
     init(mapsService: MapsService) {
+        self.mapsService = mapsService
         mapsService.add(self)
     }
-    
+
     func makeViewModelForMap(identifier: Map2.Identifier, completionHandler: @escaping (MapDetailViewModel) -> Void) {
         guard let map = maps.first(where: { $0.identifier == identifier }) else { return }
-        
-        var viewModel = ViewModel()
-        viewModel.mapName = map.location
-        completionHandler(viewModel)
+
+        mapsService.fetchImagePNGDataForMap(identifier: identifier) { (mapGraphicData) in
+            var viewModel = ViewModel()
+            viewModel.mapImagePNGData = mapGraphicData
+            viewModel.mapName = map.location
+            completionHandler(viewModel)
+        }
     }
-    
+
     func mapsServiceDidChangeMaps(_ maps: [Map2]) {
         self.maps = maps
     }
-    
+
 }
--- EurofurenceTests/Presenter Tests/Map Detail/Interactor Tests/WhenPreparingViewModel_MapDetailInteractorShould.swift
+++ EurofurenceTests/Presenter Tests/Map Detail/Interactor Tests/WhenPreparingViewModel_MapDetailInteractorShould.swift
@@ -21,4 +21,14 @@ class WhenPreparingViewModel_MapDetailInteractorShould: XCTestCase {
         XCTAssertEqual(randomMap.element.location, viewModel?.mapName)
     }
     
+    func testPrepareViewModelWithMapGraphicData() {
+        let mapsService = FakeMapsService()
+        let randomMap = mapsService.maps.randomElement()
+        let interactor = DefaultMapDetailInteractor(mapsService: mapsService)
+        var viewModel: MapDetailViewModel?
+        interactor.makeViewModelForMap(identifier: randomMap.element.identifier) { viewModel = $0 }
+        
+        XCTAssertEqual(mapsService.imagePNGDataForMap(identifier: randomMap.element.identifier), viewModel?.mapImagePNGData)
+    }
+    
 }
