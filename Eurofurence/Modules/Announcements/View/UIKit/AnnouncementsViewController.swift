d5ec6122064505e2d43082db57e816f50b4ef9ee
ShezHsky <shezhsky@icloud.com>
2018-07-02 21:38:33 +0100

    Wire up scene ports on announcements module

--- Eurofurence/Modules/Announcements/View/UIKit/AnnouncementsViewController.swift
+++ Eurofurence/Modules/Announcements/View/UIKit/AnnouncementsViewController.swift
@@ -10,10 +10,30 @@ import UIKit
 
 class AnnouncementsViewController: UIViewController, AnnouncementsScene {
 
+    // MARK: Properties
+
+    @IBOutlet weak var tableView: UITableView!
+    private var tableController: TableController? {
+        didSet {
+            tableView.dataSource = tableController
+            tableView.delegate = tableController
+        }
+    }
+
+    // MARK: Overrides
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView.register(AnnouncementTableViewCell.self)
+        delegate?.announcementsSceneDidLoad()
+    }
+
     // MARK: AnnouncementsScene
 
+    private var delegate: AnnouncementsSceneDelegate?
     func setDelegate(_ delegate: AnnouncementsSceneDelegate) {
-
+        self.delegate = delegate
     }
 
     func setAnnouncementsTitle(_ title: String) {
@@ -21,6 +41,30 @@ class AnnouncementsViewController: UIViewController, AnnouncementsScene {
     }
 
     func bind(numberOfAnnouncements: Int, using binder: AnnouncementsBinder) {
+        tableController = TableController(numberOfAnnouncements: numberOfAnnouncements, binder: binder)
+    }
+
+    // MARK: Private
+
+    private class TableController: NSObject, UITableViewDataSource, UITableViewDelegate {
+
+        private let numberOfAnnouncements: Int
+        private let binder: AnnouncementsBinder
+
+        init(numberOfAnnouncements: Int, binder: AnnouncementsBinder) {
+            self.numberOfAnnouncements = numberOfAnnouncements
+            self.binder = binder
+        }
+
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return numberOfAnnouncements
+        }
+
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell = tableView.dequeue(AnnouncementTableViewCell.self)
+            binder.bind(cell, at: indexPath.row)
+            return cell
+        }
 
     }
 
