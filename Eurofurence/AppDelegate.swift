8cb2563b8b5fde9f7ce3859bc2241e5b021cc408
ShezHsky <shezhsky@icloud.com>
2018-07-05 22:27:28 +0100

    Add bare minimum debug controller
    Tap anywhere in the app 5 times with two fingers to bring up

--- Eurofurence/AppDelegate.swift
+++ Eurofurence/AppDelegate.swift
@@ -74,10 +74,29 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         buttonsInsideSearchBarAppearance.setTitleTextAttributes(whiteTextAttributes, for: .normal)
     }
 
+    private func installDebugModuleIntoWindow() {
+        guard let window = window else { return }
+
+        let complicatedGesture = UITapGestureRecognizer(target: self, action: #selector(showDebugMenu))
+        complicatedGesture.numberOfTouchesRequired = 2
+        complicatedGesture.numberOfTapsRequired = 5
+        window.addGestureRecognizer(complicatedGesture)
+    }
+
+    @objc private func showDebugMenu(_ sender: UIGestureRecognizer) {
+        let storyboard = UIStoryboard(name: "Debug", bundle: .main)
+        guard let viewController = storyboard.instantiateInitialViewController() else { return }
+
+        let host = UINavigationController(rootViewController: viewController)
+        host.modalPresentationStyle = .formSheet
+        window?.rootViewController?.present(host, animated: true)
+    }
+
 	func application(_ application: UIApplication,
 	                 didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         ScreenshotAssistant.prepare()
         FirebaseApp.configure()
+        installDebugModuleIntoWindow()
 
 		try! ContextResolver.container.bootstrap()
 		try! ServiceResolver.container.bootstrap()
---
+++ Eurofurence/Modules/Debug/DebugTableViewController.swift
@@ -0,0 +1,51 @@
+//
+//  DebugTableViewController.swift
+//  Eurofurence
+//
+//  Created by Thomas Sherwood on 05/07/2018.
+//  Copyright © 2018 Eurofurence. All rights reserved.
+//
+
+import MobileCoreServices
+import FirebaseMessaging
+import UIKit
+
+class DebugTableViewController: UITableViewController {
+
+    // MARK: Properties
+
+    @IBOutlet weak var fcmTokenLabel: UILabel!
+    private lazy var actions: [IndexPath : () -> Void] = [
+        IndexPath(item: 0, section: 0): self.copyFCMToPasteBoard
+    ]
+
+    // MARK: Overrides
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        guard let value = Messaging.messaging().fcmToken else { return }
+        fcmTokenLabel.text = value
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        actions[indexPath]?()
+    }
+
+    // MARK: Functions
+
+    @IBAction func doneButtonTapped(_ sender: Any) {
+        dismiss(animated: true)
+    }
+
+    private func copyFCMToPasteBoard() {
+        guard let value = Messaging.messaging().fcmToken else { return }
+        UIPasteboard.general.setValue(value, forPasteboardType: kUTTypeRTF as String)
+
+        let alert = UIAlertController(title: "FCM Copied", message: nil, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .cancel))
+        present(alert, animated: true)
+    }
+
+}
