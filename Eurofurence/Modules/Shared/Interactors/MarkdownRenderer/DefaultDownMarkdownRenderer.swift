04eb6a0df2df4f20a615f1e31d29f00fc896fce7
Fenrikur <ef-app-ios@fenrikur.de>
2018-08-01 22:12:50 +0200

    #154: Added Default (Body text style) and SubtleDownMarkdownRenderer (Caption 2 text style with light grey foreground colour) with stylesheets for Dynamic Type support

---
+++ Eurofurence/Modules/Shared/Interactors/MarkdownRenderer/DefaultDownMarkdownRenderer.swift
@@ -0,0 +1,13 @@
+//
+//  DefaultDownMarkdownRenderer.swift
+//  Eurofurence
+//
+//  Created by Fenrikur Sionnar on 01.08.18.
+//  Copyright © 2018 Eurofurence. All rights reserved.
+//
+
+import Foundation
+
+struct DefaultDownMarkdownRenderer: DownMarkdownRenderer {
+	let stylesheet: String? = "* { font: -apple-system-body; } strong { font-weight: bold; }"
+}
---
+++ Eurofurence/Modules/Shared/Interactors/MarkdownRenderer/SubtleDownMarkdownRenderer.swift
@@ -0,0 +1,14 @@
+//
+//  AnnouncementsDownMarkdownRenderer.swift
+//  Eurofurence
+//
+//  Created by Dominik Schöner on 26/07/18.
+//  Copyright © 2018 Eurofurence. All rights reserved.
+//
+
+import Foundation
+import Down
+
+struct SubtleDownMarkdownRenderer: DownMarkdownRenderer {
+	let stylesheet: String? =  "* { font: -apple-system-caption2; color: #7f7f7f; } strong { font-weight: bold; }"
+}
