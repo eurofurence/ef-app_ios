d47636ca1a318962ed498e357814246cb2cd208b
Fenrikur <ef-app-ios@fenrikur.de>
2018-08-09 21:29:35 +0200

    Added support for headings (h1-h5) in Markdown

--- Eurofurence/Modules/Shared/Interactors/MarkdownRenderer/DefaultDownMarkdownRenderer.swift
+++ Eurofurence/Modules/Shared/Interactors/MarkdownRenderer/DefaultDownMarkdownRenderer.swift
@@ -9,5 +9,5 @@
 import Foundation
 
 struct DefaultDownMarkdownRenderer: DownMarkdownRenderer {
-	let stylesheet: String? = "* { font: -apple-system-body; } strong { font-weight: bold; } em { font-style: italic; }"
+	let stylesheet: String? = "* { font: -apple-system-body; } h1, h2, h3, h4, h5, h6, strong { font-weight: bold; } em { font-style: italic; } h1 { font-size: 175%; } h2 { font-size: 150%; } h3 { font-size: 130%; } h4 { font-size: 115%; } h5 { font-style: italic; }"
 }
--- Eurofurence/Modules/Shared/Interactors/MarkdownRenderer/SubtleDownMarkdownRenderer.swift
+++ Eurofurence/Modules/Shared/Interactors/MarkdownRenderer/SubtleDownMarkdownRenderer.swift
@@ -10,5 +10,5 @@ import Foundation
 import Down
 
 struct SubtleDownMarkdownRenderer: DownMarkdownRenderer {
-	let stylesheet: String? =  "* { font: -apple-system-caption2; color: #7f7f7f; } strong { font-weight: bold; } em { font-style: italic; }"
+	let stylesheet: String? =  "* { font: -apple-system-caption2; color: #7f7f7f; } h1, h2, h3, h4, h5, h6, strong { font-weight: bold; } em { font-style: italic; } h1 { font-size: 175%; } h2 { font-size: 150%; } h3 { font-size: 130%; } h4 { font-size: 115%; } h5 { font-style: italic; }"
 }
