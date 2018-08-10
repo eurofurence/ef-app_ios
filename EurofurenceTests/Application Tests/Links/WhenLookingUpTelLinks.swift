babc5bc71b3b96079a08c0deb5923848a47f13a0
Fenrikur <ef-app-ios@fenrikur.de>
2018-08-10 20:37:50 +0200

    Added test for tel: URLs to be opened as external links

---
+++ EurofurenceTests/Application Tests/Links/WhenLookingUpTelLinks.swift
@@ -0,0 +1,23 @@
+//
+//  WhenLookingUpTelLinks.swift
+//  EurofurenceTests
+//
+//  Created by Dominik Schöner on 10/08/2018.
+//  Copyright © 2018 Eurofurence. All rights reserved.
+//
+
+@testable import Eurofurence
+import XCTest
+
+class WhenLookingUpTelLinks: XCTestCase {
+	
+	func testTheAppProvidesTheExternalURL() {
+		let context = ApplicationTestBuilder().build()
+		let expected = URL(string: "tel:+1234567890")!
+		let link = Link(name: .random, type: .webExternal, contents: expected.absoluteString)
+		let action = context.application.lookupContent(for: link)
+		
+		XCTAssertEqual(.externalURL(expected), action)
+	}
+	
+}
