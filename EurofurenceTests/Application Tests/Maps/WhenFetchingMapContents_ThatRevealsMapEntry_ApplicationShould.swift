91a0fea7b313453e5732f6f73f0e193ba8e43656
Fenrikur <ef-app-ios@fenrikur.de>
2018-08-17 18:56:14 +0200

    Validate that map locations are generated correctly when retrieving map content from application

---
+++ EurofurenceTests/Application Tests/Maps/WhenFetchingMapContents_ThatRevealsMapEntry_ApplicationShould.swift
@@ -0,0 +1,33 @@
+//
+//  WhenFetchingMapContents_ThatRevealsMapEntry_ApplicationShould.swift
+//  EurofurenceTests
+//
+//  Created by Dominik Schöner on 17/08/2018.
+//  Copyright © 2018 Eurofurence. All rights reserved.
+//
+
+@testable import Eurofurence
+import XCTest
+
+class WhenFetchingMapContents_ThatRevealsMapEntry_ApplicationShould: XCTestCase {
+	
+	func testProvideTheDealer() {
+		let context = ApplicationTestBuilder().build()
+		var syncResponse = APISyncResponse.randomWithoutDeletions
+		let expectedMapEntry = APIMap.Entry(identifier: .random, x: Int.random, y: Int.random, tapRadius: Int.random, links: [])
+		var map = APIMap.random
+		let link = APIMap.Entry.Link(type: .mapEntry, name: .random, target: expectedMapEntry.identifier)
+		let (x, y, tapRadius) = (Int.random, Int.random, Int.random)
+		let entry = APIMap.Entry(identifier: .random, x: x, y: y, tapRadius: tapRadius, links: [link])
+		map.entries = [entry, expectedMapEntry]
+		syncResponse.maps.changed = [map]
+		context.performSuccessfulSync(response: syncResponse)
+		
+		var content: Map2.Content?
+		context.application.fetchContent(for: Map2.Identifier(map.identifier), atX: x, y: y) { content = $0 }
+		let expected = Map2.Content.location(x: Float(expectedMapEntry.x), y: Float(expectedMapEntry.y), name: link.name)
+		
+		XCTAssertEqual(expected, content)
+	}
+	
+}
