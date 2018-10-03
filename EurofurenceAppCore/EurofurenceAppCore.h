a215e7efbbcc99cf49dbc7dc7fcc1ab26467ae0b
ShezHsky <shezhsky@icloud.com>
2018-10-03 21:49:48 +0100

    Add framework target for app core

---
+++ EurofurenceAppCore/EurofurenceAppCore.h
@@ -0,0 +1,19 @@
+//
+//  EurofurenceAppCore.h
+//  EurofurenceAppCore
+//
+//  Created by Thomas Sherwood on 03/10/2018.
+//  Copyright © 2018 Eurofurence. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EurofurenceAppCore.
+FOUNDATION_EXPORT double EurofurenceAppCoreVersionNumber;
+
+//! Project version string for EurofurenceAppCore.
+FOUNDATION_EXPORT const unsigned char EurofurenceAppCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EurofurenceAppCore/PublicHeader.h>
+
+
