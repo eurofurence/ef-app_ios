9619ba67640c653b71bbd589a9afaccda1c00a9b
ShezHsky <shezhsky@icloud.com>
2018-10-03 12:56:21 +0100

    Organise core dependencies into subgroup
    Identifying which dependencies should/should not be injectable, or whether we have unnecessary duplication

--- Eurofurence/Application/API/Configurable Dependencies/ConventionStartDateRepository.swift
+++ Eurofurence/Application/API/Configurable Dependencies/ConventionStartDateRepository.swift
@@ -8,6 +8,9 @@
 
 import Foundation
 
+// TODO: Promote into fetching from remote config
+// 1. Make acquiring start date async as-is (API change)
+// 2. Add second adapter for fetching value from Firebase
 protocol ConventionStartDateRepository {
 
     var conventionStartDate: Date { get }
