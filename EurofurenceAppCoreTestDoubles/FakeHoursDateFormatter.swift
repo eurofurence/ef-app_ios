06d3553bddc9625456249f60b5de36767b9f4f19
ShezHsky <shezhsky@icloud.com>
2018-10-09 20:24:06 +0100

    Move fake hours formatter into core test doubles

---
+++ EurofurenceAppCoreTestDoubles/FakeHoursDateFormatter.swift
@@ -0,0 +1,32 @@
+//
+//  FakeHoursDateFormatter.swift
+//  EurofurenceAppCoreTestDoubles
+//
+//  Created by Thomas Sherwood on 09/10/2018.
+//  Copyright © 2018 Eurofurence. All rights reserved.
+//
+
+import EurofurenceAppCore
+import RandomDataGeneration
+
+public class FakeHoursDateFormatter: HoursDateFormatter {
+    
+    public init() {
+        
+    }
+    
+    private var strings = [Date : String]()
+    
+    public func hoursString(from date: Date) -> String {
+        var output = String.random
+        if let previous = strings[date] {
+            output = previous
+        }
+        else {
+            strings[date] = output
+        }
+        
+        return output
+    }
+    
+}
--- EurofurenceTests/Presenter Tests/News/Interactor Tests/DefaultNewsInteractorTestBuilder.swift
+++ EurofurenceTests/Presenter Tests/News/Interactor Tests/DefaultNewsInteractorTestBuilder.swift
@@ -12,24 +12,6 @@ import EurofurenceAppCoreTestDoubles
 import Foundation
 import XCTest
 
-class FakeHoursDateFormatter: HoursDateFormatter {
-    
-    private var strings = [Date : String]()
-    
-    func hoursString(from date: Date) -> String {
-        var output = String.random
-        if let previous = strings[date] {
-            output = previous
-        }
-        else {
-            strings[date] = output
-        }
-        
-        return output
-    }
-    
-}
-
 class CapturingRefreshService: RefreshService {
     
     private(set) var toldToRefresh = false
