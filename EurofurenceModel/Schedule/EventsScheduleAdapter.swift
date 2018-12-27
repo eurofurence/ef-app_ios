dfcef88a4dbf5185814b6ad6f390b85418813958
ShezHsky <shezhsky@icloud.com>
2018-12-27 16:51:38 +0000

    Move schedule adapter into its own file
    And group schedule types under common group

---
+++ EurofurenceModel/Schedule/EventsScheduleAdapter.swift
@@ -0,0 +1,144 @@
+//
+//  EventsScheduleAdapter.swift
+//  EurofurenceModel
+//
+//  Created by Thomas Sherwood on 27/12/2018.
+//  Copyright © 2018 Eurofurence. All rights reserved.
+//
+
+import Foundation
+
+private protocol EventFilter {
+    
+    func shouldFilter(event: APIEvent) -> Bool
+    
+}
+
+class EventsScheduleAdapter: EventsSchedule, EventConsumer {
+    
+    private let schedule: Schedule
+    private let clock: Clock
+    private var events = [EurofurenceModel.Event]()
+    private var days = [Day]()
+    private var filters = [EventFilter]()
+    private var currentDay: Day? {
+        didSet {
+            delegate?.currentEventDayDidChange(to: currentDay)
+        }
+    }
+    
+    private struct DayRestrictionFilter: EventFilter {
+        
+        var day: APIConferenceDay
+        
+        func shouldFilter(event: APIEvent) -> Bool {
+            return event.dayIdentifier == day.identifier
+        }
+        
+    }
+    
+    private struct UpdateCurrentDayWhenSignificantTimePasses: EventConsumer {
+        
+        var scheduleAdapter: EventsScheduleAdapter
+        
+        func consume(event: DomainEvent.SignificantTimePassedEvent) {
+            scheduleAdapter.updateCurrentDay()
+        }
+        
+    }
+    
+    init(schedule: Schedule, clock: Clock, eventBus: EventBus) {
+        self.schedule = schedule
+        self.clock = clock
+        events = schedule.eventModels
+        days = schedule.dayModels
+        
+        eventBus.subscribe(consumer: self)
+        eventBus.subscribe(consumer: UpdateCurrentDayWhenSignificantTimePasses(scheduleAdapter: self))
+        regenerateSchedule()
+        updateCurrentDay()
+    }
+    
+    private var delegate: EventsScheduleDelegate?
+    func setDelegate(_ delegate: EventsScheduleDelegate) {
+        self.delegate = delegate
+        
+        delegate.scheduleEventsDidChange(to: events)
+        updateDelegateWithAllDays()
+        delegate.currentEventDayDidChange(to: currentDay)
+    }
+    
+    func restrictEvents(to day: Day) {
+        guard let day = findDay(for: day.date) else { return }
+        restrictScheduleToEvents(on: day)
+    }
+    
+    private func restrictScheduleToEvents(on day: APIConferenceDay) {
+        if let idx = filters.index(where: { $0 is DayRestrictionFilter }) {
+            let filter = filters[idx] as! DayRestrictionFilter
+            guard filter.day != day else { return }
+            filters.remove(at: idx)
+        }
+        
+        let filter = DayRestrictionFilter(day: day)
+        filters.append(filter)
+        
+        regenerateSchedule()
+    }
+    
+    private func updateCurrentDay() {
+        if let day = findDay(for: clock.currentDate) {
+            currentDay = Day(date: day.date)
+            restrictScheduleToEvents(on: day)
+        } else {
+            currentDay = nil
+            
+            if let firstDay = schedule.days.sorted(by: { $0.date < $1.date }).first {
+                restrictScheduleToEvents(on: firstDay)
+            }
+        }
+    }
+    
+    private func regenerateSchedule() {
+        var allEvents = schedule.events
+        filters.forEach { (filter) in
+            allEvents = allEvents.filter(filter.shouldFilter)
+        }
+        
+        events = allEvents.compactMap(schedule.makeEventModel)
+        delegate?.scheduleEventsDidChange(to: events)
+    }
+    
+    private func findDay(for date: Date) -> APIConferenceDay? {
+        let dateOnlyComponents = resolveDateOnlyComponents(from: date)
+        
+        return schedule.days.first { (day) in
+            let dayComponents = resolveDateOnlyComponents(from: day.date)
+            return dayComponents == dateOnlyComponents
+        }
+    }
+    
+    private func resolveDateOnlyComponents(from date: Date) -> DateComponents {
+        let dateCalendarComponents: Set<Calendar.Component> = Set([.day, .month, .year])
+        let calendar = Calendar.current
+        return calendar.dateComponents(dateCalendarComponents, from: date)
+    }
+    
+    private func updateDelegateWithAllDays() {
+        delegate?.eventDaysDidChange(to: schedule.dayModels)
+    }
+    
+    func consume(event: Schedule.ChangedEvent) {
+        regenerateSchedule()
+        
+        if days != schedule.dayModels {
+            self.days = schedule.dayModels
+            updateDelegateWithAllDays()
+        }
+        
+        if filters.contains(where: { $0 is DayRestrictionFilter }) == false {
+            updateCurrentDay()
+        }
+    }
+    
+}
