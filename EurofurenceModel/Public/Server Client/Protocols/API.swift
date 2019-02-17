a05a80591908a8dac3847a17d455188cb2e62ca7
ShezHsky <shezhsky@icloud.com>
2019-02-17 20:32:44 +0000

    Declare API type to represent all current API protocols

---
+++ EurofurenceModel/Public/Server Client/Protocols/API.swift
@@ -0,0 +1,11 @@
+//
+//  API.swift
+//  EurofurenceModel
+//
+//  Created by Thomas Sherwood on 17/02/2019.
+//  Copyright © 2019 Eurofurence. All rights reserved.
+//
+
+import Foundation
+
+public protocol API: ImageAPI, LoginAPI, PrivateMessagesAPI, SyncAPI {}
