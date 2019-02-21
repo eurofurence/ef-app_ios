f9fc40c5e1a0b59ae0c8fd1bb2949ec828e5feed
ShezHsky <shezhsky@icloud.com>
2019-02-21 15:18:30 +0000

    Add protocol type to represent event view model

--- Eurofurence/Modules/Schedule/Interactor/ScheduleEventViewModel.swift
+++ Eurofurence/Modules/Schedule/Interactor/ScheduleEventViewModel.swift
@@ -8,7 +8,25 @@
 
 import Foundation
 
-struct ScheduleEventViewModel: Equatable {
+protocol ScheduleEventViewModelProtocol {
+
+    var title: String { get }
+    var startTime: String { get }
+    var endTime: String { get }
+    var location: String { get }
+    var bannerGraphicPNGData: Data? { get }
+    var isFavourite: Bool { get }
+    var isSponsorOnly: Bool { get }
+    var isSuperSponsorOnly: Bool { get }
+    var isArtShow: Bool { get }
+    var isKageEvent: Bool { get }
+    var isDealersDenEvent: Bool { get }
+    var isMainStageEvent: Bool { get }
+    var isPhotoshootEvent: Bool { get }
+
+}
+
+struct ScheduleEventViewModel: ScheduleEventViewModelProtocol, Equatable {
 
     var title: String
     var startTime: String
