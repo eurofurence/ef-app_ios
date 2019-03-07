1a462db17545659a686333dba131ce4c886dd513
ShezHsky <shezhsky@icloud.com>
2019-03-07 10:46:51 +0000

    Resolve file length warning for news interactor

--- Eurofurence/Modules/News/Interactor/DefaultNewsInteractor.swift
+++ Eurofurence/Modules/News/Interactor/DefaultNewsInteractor.swift
@@ -9,16 +9,6 @@
 import EurofurenceModel
 import Foundation
 
-private protocol NewsViewModelComponent {
-
-    var childCount: Int { get }
-    var title: String { get }
-
-    func announceContent(at index: Int, to visitor: NewsViewModelVisitor)
-    func announceValue(at index: Int, to completionHandler: @escaping (NewsViewModelValue) -> Void)
-
-}
-
 class DefaultNewsInteractor: NewsInteractor,
                              AnnouncementsServiceObserver,
                              AuthenticationStateObserver,
@@ -232,20 +222,20 @@ class DefaultNewsInteractor: NewsInteractor,
     private func regenerateViewModel() {
         let userWidgetViewModel = makeUserWidgetViewModel()
         var components = [NewsViewModelComponent]()
-        components.append(UserComponent(viewModel: userWidgetViewModel))
+        components.append(NewsViewModelUserComponent(viewModel: userWidgetViewModel))
 
         if let daysUntilConvention = daysUntilConvention {
-            components.append(CountdownComponent(daysUntilConvention: daysUntilConvention))
+            components.append(NewsViewModelCountdownComponent(daysUntilConvention: daysUntilConvention))
         }
 
-        let announcementsComponent = AnnouncementsComponent(announcements: announcements,
+        let announcementsComponent = NewsViewModelAnnouncementsComponent(announcements: announcements,
                                                             readAnnouncements: readAnnouncements,
                                                             announcementsDateFormatter: announcementsDateFormatter,
                                                             markdownRenderer: announcementsMarkdownRenderer)
         components.append(announcementsComponent)
 
         if !upcomingEvents.isEmpty {
-            components.append(EventsComponent(title: .upcomingEvents,
+            components.append(NewsViewModelEventsComponent(title: .upcomingEvents,
                                               events: upcomingEvents,
                                               favouriteEventIdentifiers: favouriteEventIdentifiers,
                                               startTimeFormatter: { (event) -> String in
@@ -256,7 +246,7 @@ class DefaultNewsInteractor: NewsInteractor,
         }
 
         if !runningEvents.isEmpty {
-            components.append(EventsComponent(title: .runningEvents,
+            components.append(NewsViewModelEventsComponent(title: .runningEvents,
                                               events: runningEvents,
                                               favouriteEventIdentifiers: favouriteEventIdentifiers,
                                               startTimeFormatter: { (_) -> String in
@@ -265,7 +255,7 @@ class DefaultNewsInteractor: NewsInteractor,
         }
 
         if !favouriteEvents.isEmpty {
-            components.append(EventsComponent(title: .todaysFavouriteEvents,
+            components.append(NewsViewModelEventsComponent(title: .todaysFavouriteEvents,
                                               events: favouriteEvents,
                                               favouriteEventIdentifiers: favouriteEventIdentifiers,
                                               startTimeFormatter: { (event) -> String in
@@ -273,7 +263,7 @@ class DefaultNewsInteractor: NewsInteractor,
             }, hoursDateFormatter: hoursDateFormatter))
         }
 
-        let viewModel = ViewModel(components: components)
+        let viewModel = DefaultNewsViewModel(components: components)
         delegate?.viewModelDidUpdate(viewModel)
     }
 
@@ -287,177 +277,6 @@ class DefaultNewsInteractor: NewsInteractor,
                                                 detailedPrompt: .anonymousUserLoginDescription,
                                                 hasUnreadMessages: false)
         }
-    }
-
-    private struct UserComponent: NewsViewModelComponent {
-
-        private let viewModel: UserWidgetComponentViewModel
-
-        init(viewModel: UserWidgetComponentViewModel) {
-            self.viewModel = viewModel
-        }
-
-        var childCount: Int { return 1 }
-        var title: String { return .yourEurofurence }
-
-        func announceContent(at index: Int, to visitor: NewsViewModelVisitor) {
-            visitor.visit(viewModel)
-        }
-
-        func announceValue(at index: Int, to completionHandler: @escaping (NewsViewModelValue) -> Void) {
-            completionHandler(.messages)
-        }
-
-    }
-
-    private struct CountdownComponent: NewsViewModelComponent {
-
-        private let viewModel: ConventionCountdownComponentViewModel
-
-        init(daysUntilConvention: Int) {
-            let message = String.daysUntilConventionMessage(days: daysUntilConvention)
-            viewModel = ConventionCountdownComponentViewModel(timeUntilConvention: message)
-        }
-
-        var childCount: Int {
-            return 1
-        }
-
-        var title: String {
-            return .daysUntilConvention
-        }
-
-        func announceContent(at index: Int, to visitor: NewsViewModelVisitor) {
-            visitor.visit(viewModel)
-        }
-
-        func announceValue(at index: Int, to completionHandler: @escaping (NewsViewModelValue) -> Void) {
-
-        }
-
-    }
-
-    private struct AnnouncementsComponent: NewsViewModelComponent {
-
-        private let announcements: [Announcement]
-        private let viewModels: [AnnouncementComponentViewModel]
-
-        init(announcements: [Announcement],
-             readAnnouncements: [AnnouncementIdentifier],
-             announcementsDateFormatter: AnnouncementDateFormatter,
-             markdownRenderer: MarkdownRenderer) {
-            self.announcements = announcements
-            viewModels = announcements.map({ (announcement) -> AnnouncementComponentViewModel in
-                return AnnouncementComponentViewModel(title: announcement.title,
-                                                      detail: markdownRenderer.render(announcement.content),
-                                                      receivedDateTime: announcementsDateFormatter.string(from: announcement.date),
-                                                      isRead: readAnnouncements.contains(announcement.identifier))
-            })
-        }
-
-        var childCount: Int { return viewModels.count + 1 }
-        var title: String { return .announcements }
-
-        func announceContent(at index: Int, to visitor: NewsViewModelVisitor) {
-            switch index {
-            case 0:
-                visitor.visit(ViewAllAnnouncementsComponentViewModel(caption: .allAnnouncements))
-
-            default:
-                let viewModel = viewModels[index - 1]
-                visitor.visit(viewModel)
-            }
-        }
-
-        func announceValue(at index: Int, to completionHandler: @escaping (NewsViewModelValue) -> Void) {
-            switch index {
-            case 0:
-                completionHandler(.allAnnouncements)
-
-            default:
-                let announcement = announcements[index - 1]
-                completionHandler(.announcement(announcement.identifier))
-            }
-        }
-
-    }
-
-    private struct EventsComponent: NewsViewModelComponent {
-
-        private let events: [Event]
-        private let viewModels: [EventComponentViewModel]
-
-        init(title: String,
-             events: [Event],
-             favouriteEventIdentifiers: [EventIdentifier],
-             startTimeFormatter: (Event) -> String,
-             hoursDateFormatter: HoursDateFormatter) {
-            self.title = title
-            self.events = events
-
-            viewModels = events.map { (event) -> EventComponentViewModel in
-                return EventComponentViewModel(startTime: startTimeFormatter(event),
-                                               endTime: hoursDateFormatter.hoursString(from: event.endDate),
-                                               eventName: event.title,
-                                               location: event.room.name,
-                                               isSponsorEvent: event.isSponsorOnly,
-                                               isSuperSponsorEvent: event.isSuperSponsorOnly,
-                                               isFavourite: favouriteEventIdentifiers.contains(event.identifier),
-                                               isArtShowEvent: event.isArtShow,
-                                               isKageEvent: event.isKageEvent,
-                                               isDealersDenEvent: event.isDealersDen,
-                                               isMainStageEvent: event.isMainStage,
-                                               isPhotoshootEvent: event.isPhotoshoot)
-            }
-        }
-
-        var title: String
-
-        var childCount: Int {
-            return viewModels.count
-        }
-
-        func announceContent(at index: Int, to visitor: NewsViewModelVisitor) {
-            let viewModel = viewModels[index]
-            visitor.visit(viewModel)
-        }
-
-        func announceValue(at index: Int, to completionHandler: @escaping (NewsViewModelValue) -> Void) {
-            let event = events[index]
-            completionHandler(.event(event))
-        }
-
-    }
-
-    private struct ViewModel: NewsViewModel {
-
-        private let components: [NewsViewModelComponent]
-
-        init(components: [NewsViewModelComponent]) {
-            self.components = components
-        }
-
-        var numberOfComponents: Int {
-            return components.count
-        }
-
-        func numberOfItemsInComponent(at index: Int) -> Int {
-            return components[index].childCount
-        }
-
-        func titleForComponent(at index: Int) -> String {
-            return components[index].title
-        }
-
-        func describeComponent(at indexPath: IndexPath, to visitor: NewsViewModelVisitor) {
-            let component = components[indexPath.section]
-            component.announceContent(at: indexPath.item, to: visitor)
-        }
-
-        func fetchModelValue(at indexPath: IndexPath, completionHandler: @escaping (NewsViewModelValue) -> Void) {
-            components[indexPath.section].announceValue(at: indexPath.item, to: completionHandler)
-        }
-
     }
 
 }
