0c3186c2535ff6ae6affc6c1cc9c7a5c7286b79e
ShezHsky <shezhsky@icloud.com>
2019-03-18 21:00:46 +0000

    Add extension onto String to produce base64 encoded variant

---
+++ EurofurenceModel/Public/String+Base64.swift
@@ -0,0 +1,17 @@
+//
+//  String+Base64.swift
+//  EurofurenceModel
+//
+//  Created by Thomas Sherwood on 18/03/2019.
+//  Copyright © 2019 Eurofurence. All rights reserved.
+//
+
+import Foundation
+
+public extension String {
+    
+    public var base64EncodedString: String? {
+        return data(using: .utf8)?.base64EncodedString()
+    }
+    
+}
