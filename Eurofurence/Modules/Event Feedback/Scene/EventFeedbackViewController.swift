b4c5ff6d107a0200ded4cf057f02760ab767973f
ShezHsky <shezhsky@icloud.com>
2019-04-14 21:49:06 +0100

    Hook up additional WIP parts of feedback view model

--- Eurofurence/Modules/Event Feedback/Scene/EventFeedbackViewController.swift
+++ Eurofurence/Modules/Event Feedback/Scene/EventFeedbackViewController.swift
@@ -1,22 +1,28 @@
 import UIKit
 
-class EventFeedbackViewController: UITableViewController, EventFeedbackScene {
+class EventFeedbackViewController: UITableViewController, UITextViewDelegate, EventFeedbackScene {
     
-    @IBOutlet weak var eventTitleLabel: UILabel!
-    @IBOutlet weak var eventSubheadingLabel: UILabel!
-    @IBOutlet weak var feedbackTextView: UITextView!
+    @IBOutlet private weak var eventTitleLabel: UILabel!
+    @IBOutlet private weak var eventSubheadingLabel: UILabel!
+    @IBOutlet private weak var feedbackTextView: UITextView!
 
     @IBAction func cancelFeedback(_ sender: Any) {
         
     }
     
     @IBAction func submitFeedback(_ sender: Any) {
-        
+        viewModel?.submitFeedback()
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        feedbackTextView.delegate = self
         delegate?.eventFeedbackSceneDidLoad()
+    }
+    
+    func textViewDidChange(_ textView: UITextView) {
+        viewModel?.feedbackChanged(textView.text)
     }
     
     private var delegate: EventFeedbackSceneDelegate?
@@ -24,7 +30,10 @@ class EventFeedbackViewController: UITableViewController, EventFeedbackScene {
         self.delegate = delegate
     }
     
+    private var viewModel: EventFeedbackViewModel?
     func bind(_ viewModel: EventFeedbackViewModel) {
+        self.viewModel = viewModel
+        
         eventTitleLabel.text = viewModel.eventTitle
         eventSubheadingLabel.text = [viewModel.eventDayAndTime, viewModel.eventLocation, viewModel.eventHosts].joined(separator: "\n")
     }
