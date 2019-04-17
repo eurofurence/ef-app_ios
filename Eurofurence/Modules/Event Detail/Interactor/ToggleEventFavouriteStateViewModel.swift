070e34895d74b0b23fea652283f85ae915f2b181
ShezHsky <shezhsky@icloud.com>
2019-04-17 20:18:44 +0100

    Toggle favourite state from command

--- Eurofurence/Modules/Event Detail/Interactor/ToggleEventFavouriteStateViewModel.swift
+++ Eurofurence/Modules/Event Detail/Interactor/ToggleEventFavouriteStateViewModel.swift
@@ -1,19 +1,37 @@
 import EurofurenceModel
 
-class ToggleEventFavouriteStateViewModel: EventActionViewModel {
+class ToggleEventFavouriteStateViewModel: EventActionViewModel, EventObserver {
     
     private let event: Event
+    private var isFavourite = false
     
     init(event: Event) {
         self.event = event
+        event.add(self)
     }
     
+    func eventDidBecomeFavourite(_ event: Event) {
+        isFavourite = true
+    }
+    
+    func eventDidBecomeUnfavourite(_ event: Event) {
+        isFavourite = false
+    }
+    
+    private var visitor: EventActionViewModelVisitor?
     func describe(to visitor: EventActionViewModelVisitor) {
+        self.visitor = visitor
         visitor.visitActionTitle(.unfavourite)
     }
     
     func perform() {
-        event.unfavourite()
+        if isFavourite {
+            event.unfavourite()
+        } else {
+            event.favourite()
+        }
+        
+        visitor?.visitActionTitle(.favourite)
     }
     
 }
