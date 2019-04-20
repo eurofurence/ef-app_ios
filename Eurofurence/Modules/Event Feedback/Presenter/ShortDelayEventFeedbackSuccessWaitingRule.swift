9b95dd102a58df810bbc4fbbe74ff01994cb7806
ShezHsky <shezhsky@icloud.com>
2019-04-20 17:32:24 +0100

    Use shorthand asyncAfter syntax

--- Eurofurence/Modules/Event Feedback/Presenter/ShortDelayEventFeedbackSuccessWaitingRule.swift
+++ Eurofurence/Modules/Event Feedback/Presenter/ShortDelayEventFeedbackSuccessWaitingRule.swift
@@ -3,11 +3,7 @@ import Foundation
 struct ShortDelayEventFeedbackSuccessWaitingRule: EventFeedbackSuccessWaitingRule {
     
     func evaluateRule(handler: @escaping () -> Void) {
-        let now = DispatchTime.now()
-        let threeSeconds = DispatchTime(uptimeNanoseconds: NSEC_PER_SEC * UInt64(3))
-        let threeSecondsFromNow = DispatchTime(uptimeNanoseconds: now.rawValue + threeSeconds.rawValue)
-        
-        DispatchQueue.main.asyncAfter(deadline: threeSeconds, execute: handler)
+        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(3), execute: handler)
     }
     
 }
