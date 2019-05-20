1506e80940805591eaa4d69bf1f5d418dbc30cc8
ShezHsky <shezhsky@icloud.com>
2019-05-20 17:02:24 +0100

    Add a little animation on large button press

--- Eurofurence/Views/Controls/RoundedCornerButton.swift
+++ Eurofurence/Views/Controls/RoundedCornerButton.swift
@@ -13,5 +13,41 @@ class RoundedCornerButton: UIButton {
     override var intrinsicContentSize: CGSize {
         return CGSize(width: UIView.noIntrinsicMetric, height: 45)
     }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        transitionToPressedState()
+        super.touchesBegan(touches, with: event)
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        transitionToUnpressedState()
+        super.touchesEnded(touches, with: event)
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        transitionToUnpressedState()
+        super.touchesCancelled(touches, with: event)
+    }
+    
+    private func transitionToPressedState() {
+        let scaleFactor: CGFloat = 0.95
+        let scaleTransform = CGAffineTransform(scaleX: scaleFactor, y: scaleFactor)
+        swapTransform(to: scaleTransform)
+    }
+    
+    private func transitionToUnpressedState() {
+        swapTransform(to: .identity)
+    }
+    
+    private func swapTransform(to transform: CGAffineTransform) {
+        UIView.animate(withDuration: 0.15,
+                       delay: 0,
+                       usingSpringWithDamping: 1,
+                       initialSpringVelocity: 0,
+                       options: .beginFromCurrentState,
+                       animations: {
+            self.layer.setAffineTransform(transform)
+        })
+    }
 
 }
