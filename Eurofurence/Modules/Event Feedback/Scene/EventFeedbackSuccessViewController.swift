672e052f115e2bb4a8b77ffc8eab04576704284c
ShezHsky <shezhsky@icloud.com>
2019-06-09 19:32:00 +0100

    Add some flair to the feedback success view controller

--- Eurofurence/Modules/Event Feedback/Scene/EventFeedbackSuccessViewController.swift
+++ Eurofurence/Modules/Event Feedback/Scene/EventFeedbackSuccessViewController.swift
@@ -1,5 +1,12 @@
 import UIKit
 
 class EventFeedbackSuccessViewController: UIViewController {
+    
+    @IBOutlet private weak var tickImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tickImageView.tintColor = .eurofurenceGreen
+    }
 
 }
