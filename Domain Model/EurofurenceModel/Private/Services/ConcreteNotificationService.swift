04955e91b8c1d482663555fe4135e60e8f05310c
ShezHsky <shezhsky@icloud.com>
2019-06-27 22:37:30 +0100

    Apply chain of responsibility for handling notifications

--- Domain Model/EurofurenceModel/Private/Services/ConcreteNotificationService.swift
+++ Domain Model/EurofurenceModel/Private/Services/ConcreteNotificationService.swift
@@ -1,6 +1,152 @@
 import EventBus
 import Foundation
 
+class NotificationContentDecodingContext {
+    
+    let payload: [String: String]
+    private let completionHandler: (NotificationContent) -> Void
+    
+    init(payload: [String: String], completionHandler: @escaping (NotificationContent) -> Void) {
+        self.payload = payload
+        self.completionHandler = completionHandler
+    }
+    
+    func complete(content: NotificationContent) {
+        completionHandler(content)
+    }
+    
+}
+
+class NotificationContentDecodingChainComponent {
+    
+    private let nextComponent: NotificationContentDecodingChainComponent?
+    
+    init(nextComponent: NotificationContentDecodingChainComponent?) {
+        self.nextComponent = nextComponent
+    }
+    
+    func process(context: NotificationContentDecodingContext) {
+        nextComponent?.process(context: context)
+    }
+    
+}
+
+class EventNotificationContentDecodingChainComponent: NotificationContentDecodingChainComponent {
+    
+    private let eventsService: EventsService
+    
+    init(nextComponent: NotificationContentDecodingChainComponent?, eventsService: EventsService) {
+        self.eventsService = eventsService
+        super.init(nextComponent: nextComponent)
+    }
+    
+    override func process(context: NotificationContentDecodingContext) {
+        if isEventPayload(context) {
+            processEventPayload(context)
+        } else {
+            super.process(context: context)
+        }
+    }
+    
+    private func isEventPayload(_ context: NotificationContentDecodingContext) -> Bool {
+        return context.payload[ApplicationNotificationKey.notificationContentKind.rawValue] == ApplicationNotificationContentKind.event.rawValue
+    }
+    
+    private func processEventPayload(_ context: NotificationContentDecodingContext) {
+        let content: NotificationContent = {
+            guard let identifier = context.payload[ApplicationNotificationKey.notificationContentIdentifier.rawValue] else { return .unknown }
+            
+            let eventIdentifier = EventIdentifier(identifier)
+            guard eventsService.fetchEvent(identifier: eventIdentifier) != nil else { return .unknown }
+            
+            return .event(eventIdentifier)
+        }()
+        
+        context.complete(content: content)
+    }
+    
+}
+
+class MessageNotificationContentDecodingChainComponent: NotificationContentDecodingChainComponent {
+    
+    private let privateMessagesService: PrivateMessagesService
+    
+    init(nextComponent: NotificationContentDecodingChainComponent?, privateMessagesService: PrivateMessagesService) {
+        self.privateMessagesService = privateMessagesService
+        super.init(nextComponent: nextComponent)
+    }
+    
+    override func process(context: NotificationContentDecodingContext) {
+        if let messageIdentifier = context.payload["message_id"] {
+            let identifier = MessageIdentifier(messageIdentifier)
+            if privateMessagesService.fetchMessage(identifiedBy: identifier) != nil {
+                context.complete(content: .message(identifier))
+                return
+            }
+        }
+        
+        super.process(context: context)
+    }
+    
+}
+
+class SyncBeforeContinuingNotificationContentDecodingChainComponent: NotificationContentDecodingChainComponent {
+    
+    private let refreshService: RefreshService
+    
+    init(nextComponent: NotificationContentDecodingChainComponent?, refreshService: RefreshService) {
+        self.refreshService = refreshService
+        super.init(nextComponent: nextComponent)
+    }
+    
+    override func process(context: NotificationContentDecodingContext) {
+        refreshService.refreshLocalStore { (error) in
+            if error == nil {
+                super.process(context: context)
+            } else {
+                context.complete(content: .failedSync)
+            }
+        }
+    }
+    
+}
+
+class AnnouncementNotificationContentDecodingChainComponent: NotificationContentDecodingChainComponent {
+    
+    private let announcementsService: AnnouncementsService
+    
+    init(nextComponent: NotificationContentDecodingChainComponent?, announcementsService: AnnouncementsService) {
+        self.announcementsService = announcementsService
+        super.init(nextComponent: nextComponent)
+    }
+    
+    override func process(context: NotificationContentDecodingContext) {
+        if let announcementIdentifier = context.payload["announcement_id"] {
+            processAnnouncement(announcementIdentifier, context: context)
+        } else {
+            super.process(context: context)
+        }
+    }
+    
+    private func processAnnouncement(_ announcementIdentifier: String, context: NotificationContentDecodingContext) {
+        let identifier = AnnouncementIdentifier(announcementIdentifier)
+        if announcementsService.fetchAnnouncement(identifier: identifier) != nil {
+            context.complete(content: .announcement(identifier))
+        } else {
+            context.complete(content: .invalidatedAnnouncement)
+        }
+    }
+    
+}
+
+class SuccessfulSyncNotificationContentDecodingChainComponent: NotificationContentDecodingChainComponent {
+    
+    override func process(context: NotificationContentDecodingContext) {
+        context.complete(content: .successfulSync)
+    }
+    
+}
+
 struct ConcreteNotificationService: NotificationService {
 
     var eventBus: EventBus
@@ -10,106 +156,18 @@ struct ConcreteNotificationService: NotificationService {
     var privateMessagesService: PrivateMessagesService
     
     func handleNotification(payload: [String: String], completionHandler: @escaping (NotificationContent) -> Void) {
-        NotificationPayloadHandler(
-            payload: payload,
-            completionHandler: completionHandler,
-            eventsService: eventsService,
-            announcementsService: announcementsService,
-            refreshService: refreshService,
-            privateMessagesService: privateMessagesService
-        ).handle()
+        let catchAllChainComponent = SuccessfulSyncNotificationContentDecodingChainComponent(nextComponent: nil)
+        let findAnnouncement = AnnouncementNotificationContentDecodingChainComponent(nextComponent: catchAllChainComponent, announcementsService: announcementsService)
+        let findMessageAfterSyncComponent = MessageNotificationContentDecodingChainComponent(nextComponent: findAnnouncement, privateMessagesService: privateMessagesService)
+        let syncComponent = SyncBeforeContinuingNotificationContentDecodingChainComponent(nextComponent: findMessageAfterSyncComponent, refreshService: refreshService)
+        let findLocalMessageComponent = MessageNotificationContentDecodingChainComponent(nextComponent: syncComponent, privateMessagesService: privateMessagesService)
+        let findLocalEventComponent = EventNotificationContentDecodingChainComponent(nextComponent: findLocalMessageComponent, eventsService: eventsService)
+        let context = NotificationContentDecodingContext(payload: payload, completionHandler: completionHandler)
+        findLocalEventComponent.process(context: context)
     }
 
     func storeRemoteNotificationsToken(_ deviceToken: Data) {
         eventBus.post(DomainEvent.RemoteNotificationTokenAvailable(deviceToken: deviceToken))
-    }
-    
-    private struct NotificationPayloadHandler {
-        
-        var payload: [String: String]
-        var completionHandler: (NotificationContent) -> Void
-        var eventsService: ConcreteEventsService
-        var announcementsService: ConcreteAnnouncementsService
-        var refreshService: ConcreteRefreshService
-        var privateMessagesService: PrivateMessagesService
-        
-        private var isEventPayload: Bool {
-            return payload[ApplicationNotificationKey.notificationContentKind.rawValue] == ApplicationNotificationContentKind.event.rawValue
-        }
-        
-        func handle() {
-            if handleEventPayload() {
-                return
-            }
-            
-            if handleMessagePayload() {
-                return
-            }
-            
-            refreshService.refreshLocalStore(completionHandler: refreshDidFinish)
-        }
-        
-        private func handleEventPayload() -> Bool {
-            guard isEventPayload else { return false }
-            
-            let content: NotificationContent = {
-                guard let identifier = payload[ApplicationNotificationKey.notificationContentIdentifier.rawValue] else { return .unknown }
-                
-                let eventIdentifier = EventIdentifier(identifier)
-                guard eventsService.fetchEvent(identifier: eventIdentifier) != nil else { return .unknown }
-                
-                return .event(eventIdentifier)
-            }()
-            
-            completionHandler(content)
-            return true
-        }
-        
-        private func handleMessagePayload() -> Bool {
-            if let messageIdentifier = payload["message_id"] {
-                let identifier = MessageIdentifier(messageIdentifier)
-                if privateMessagesService.fetchMessage(identifiedBy: identifier) != nil {
-                    completionHandler(.message(identifier))
-                    return true
-                }
-            }
-            
-            return false
-        }
-        
-        private func refreshDidFinish(_ error: Error?) {
-            if handleMessagePayload() {
-                return
-            }
-            
-            if error == nil {
-                handlePostSyncSuccess()
-            } else {
-                completionHandler(.failedSync)
-            }
-        }
-        
-        private func handlePostSyncSuccess() {
-            if handleAnnouncementPayload() {
-                return
-            }
-            
-            completionHandler(.successfulSync)
-        }
-        
-        private func handleAnnouncementPayload() -> Bool {
-            guard let announcementIdentifier = payload["announcement_id"] else { return false }
-            
-            let identifier = AnnouncementIdentifier(announcementIdentifier)
-            if announcementsService.models.contains(where: { $0.identifier == identifier }) {
-                completionHandler(.announcement(identifier))
-            } else {
-                completionHandler(.invalidatedAnnouncement)
-            }
-            
-            return true
-        }
-        
     }
 
 }
