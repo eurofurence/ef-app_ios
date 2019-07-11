f5fb1a1ecacf8c5114644c139b625d4df78801c9
ShezHsky <shezhsky@icloud.com>
2019-07-11 19:56:18 +0100

    Cater for bar button items in concrete share service

--- Eurofurence/App Services/Share/ActivityShareService.swift
+++ Eurofurence/App Services/Share/ActivityShareService.swift
@@ -6,14 +6,20 @@ struct ActivityShareService: ShareService {
         let app = UIApplication.shared
         guard let optionalWindow = app.delegate?.window,
               let window = optionalWindow,
-              let rootViewController = window.rootViewController,
-              let sender = sender as? UIView else { return }
+              let rootViewController = window.rootViewController else { return }
         
-        let senderFrame = sender.frame
-        let senderCenter = CGPoint(x: senderFrame.width / 2, y: senderFrame.height / 2)
         let activityViewController = UIActivityViewController(activityItems: [item], applicationActivities: nil)
-        activityViewController.popoverPresentationController?.sourceView = sender
-        activityViewController.popoverPresentationController?.sourceRect = CGRect(origin: senderCenter, size: .zero)
+        
+        if let sender = sender as? UIView {
+            let senderFrame = sender.frame
+            let senderCenter = CGPoint(x: senderFrame.width / 2, y: senderFrame.height / 2)
+            activityViewController.popoverPresentationController?.sourceView = sender
+            activityViewController.popoverPresentationController?.sourceRect = CGRect(origin: senderCenter, size: .zero)
+        }
+        
+        if let sender = sender as? UIBarButtonItem {
+            activityViewController.popoverPresentationController?.barButtonItem = sender
+        }
         
         rootViewController.present(activityViewController, animated: true)
     }
