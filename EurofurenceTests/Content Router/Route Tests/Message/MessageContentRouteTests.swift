e0d0395f80354b34604a2cd66a5694ebb9ea67cc
ShezHsky <shezhsky@icloud.com>
2020-05-01 16:36:40 +0100

    Stub out route for showing message

---
+++ EurofurenceTests/Content Router/Route Tests/Message/MessageContentRouteTests.swift
@@ -0,0 +1,23 @@
+import Eurofurence
+import EurofurenceModel
+import XCTest
+
+class MessageContentRouteTests: XCTestCase {
+    
+    func testShowsDetailContentController() {
+        let message = MessageIdentifier.random
+        let content = MessageContentRepresentation(identifier: message)
+        let messageModuleFactory = StubMessageDetailModuleProviding()
+        let contentWireframe = CapturingContentWireframe()
+        let route = MessageContentRoute(
+            messageModuleFactory: messageModuleFactory,
+            contentWireframe: contentWireframe
+        )
+        
+        route.route(content)
+        
+        XCTAssertEqual(message, messageModuleFactory.capturedMessage)
+        XCTAssertEqual(messageModuleFactory.stubInterface, contentWireframe.presentedDetailContentController)
+    }
+
+}
