f3b7b9b781ac79439e82a5e2ac60527a1884258c
ShezHsky <shezhsky@icloud.com>
2020-05-01 19:53:39 +0100

    Directly set root view controller onto window

--- Eurofurence/Director/AppWindowWireframe.swift
+++ Eurofurence/Director/AppWindowWireframe.swift
@@ -7,60 +7,15 @@ struct AppWindowWireframe: WindowWireframe {
         guard let window = UIApplication.shared.delegate?.window, let unwrappedWindow = window else { fatalError("Application has no window") }
         return AppWindowWireframe(window: unwrappedWindow)
     }()
-
-    private let containerViewController: ContainerViewController
+    
+    private let window: UIWindow
 
     public init(window: UIWindow) {
-        containerViewController = ContainerViewController()
-        window.rootViewController = containerViewController
+        self.window = window
     }
 
     func setRoot(_ viewController: UIViewController) {
-        containerViewController.swapRoot(to: viewController)
-    }
-    
-    private class ContainerViewController: UIViewController {
-        
-        private var currentRoot: UIViewController?
-        
-        override func viewDidLoad() {
-            super.viewDidLoad()
-            view.backgroundColor = .pantone330U
-        }
-        
-        func swapRoot(to newRoot: UIViewController) {
-            embedChild(newRoot)
-            
-            UIView.animate(withDuration: 0.25, animations: {
-                newRoot.view.alpha = 1
-                self.currentRoot?.view.alpha = 0
-            }, completion: { (_) in
-                newRoot.didMove(toParent: self)
-                self.unembedChild(self.currentRoot)
-                self.currentRoot = newRoot
-            })
-        }
-        
-        private func embedChild(_ newRoot: UIViewController) {
-            newRoot.willMove(toParent: self)
-            newRoot.view.alpha = 0
-            view.addSubview(newRoot.view)
-            NSLayoutConstraint.activate([newRoot.view.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-                                         newRoot.view.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-                                         newRoot.view.topAnchor.constraint(equalTo: view.topAnchor),
-                                         newRoot.view.bottomAnchor.constraint(equalTo: view.bottomAnchor)
-                ])
-            
-            addChild(newRoot)
-        }
-        
-        private func unembedChild(_ oldRoot: UIViewController?) {
-            oldRoot?.willMove(toParent: nil)
-            oldRoot?.view.removeFromSuperview()
-            oldRoot?.removeFromParent()
-            oldRoot?.didMove(toParent: nil)
-        }
-        
+        window.rootViewController = viewController
     }
 
 }
