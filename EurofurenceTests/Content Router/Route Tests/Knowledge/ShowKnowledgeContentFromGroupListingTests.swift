4291a3de5d9828c4b43186013568b081f1a85308
ShezHsky <shezhsky@icloud.com>
2020-05-02 17:45:06 +0100

    Add route for opening knowledge entry from entries

---
+++ EurofurenceTests/Content Router/Route Tests/Knowledge/ShowKnowledgeContentFromGroupListingTests.swift
@@ -0,0 +1,17 @@
+import Eurofurence
+import EurofurenceModel
+import EurofurenceModelTestDoubles
+import XCTest
+
+class ShowKnowledgeContentFromGroupListingTests: XCTestCase {
+
+    func testShowsKnowledgeEntryContent() {
+        let knowledgeEntry = KnowledgeEntryIdentifier.random
+        let router = FakeContentRouter()
+        let navigator = ShowKnowledgeContentFromGroupListing(router: router)
+        navigator.knowledgeGroupEntriesModuleDidSelectKnowledgeEntry(identifier: knowledgeEntry)
+        
+        router.assertRouted(to: KnowledgeEntryContentRepresentation(identifier: knowledgeEntry))
+    }
+
+}
