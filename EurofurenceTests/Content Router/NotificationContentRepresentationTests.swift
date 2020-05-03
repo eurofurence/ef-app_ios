65d6079aa66d70ce0e0026ed46b0ffe43557c8b4
ShezHsky <shezhsky@icloud.com>
2020-05-03 11:40:19 +0100

    Handle decoding messages and announcements

--- EurofurenceTests/Content Router/NotificationContentRepresentationTests.swift
+++ EurofurenceTests/Content Router/NotificationContentRepresentationTests.swift
@@ -4,17 +4,43 @@ import XCTest
 
 public struct NotificationContentRepresentation: ContentRepresentation {
     
-    public var userInfo: [AnyHashable: AnyHashable]
+    private let payload: NotificationPayload
+    
+    private struct NotificationPayload: Equatable {
+        
+        private let userInfo: [AnyHashable: AnyHashable]
+        
+        init(userInfo: [AnyHashable: Any]) {
+            self.userInfo = userInfo as? [AnyHashable: AnyHashable] ?? [:]
+        }
+        
+        func value<T>(for key: AnyHashable) -> T? {
+            userInfo[key] as? T
+        }
+        
+    }
     
     public init(userInfo: [AnyHashable: Any]) {
-        self.userInfo = userInfo as? [AnyHashable: AnyHashable] ?? [:]
+        payload = NotificationPayload(userInfo: userInfo)
     }
     
     public func describe(to recipient: ContentRepresentationRecipient) {
-        if let rawEventIdentifier = userInfo[ApplicationNotificationKey.notificationContentIdentifier] as? String {
+        if let rawEventIdentifier: String = payload.value(for: ApplicationNotificationKey.notificationContentIdentifier) {
             let eventIdentifier = EventIdentifier(rawEventIdentifier)
             let eventContent = EventContentRepresentation(identifier: eventIdentifier)
             recipient.receive(eventContent)
+        }
+        
+        if let rawMessageIdentifier: String = payload.value(for: "message_id") {
+            let messageIdentifier = MessageIdentifier(rawMessageIdentifier)
+            let messageContent = MessageContentRepresentation(identifier: messageIdentifier)
+            recipient.receive(messageContent)
+        }
+        
+        if let rawAnnouncementIdentifier: String = payload.value(for: "announcement_id") {
+            let announcementIdentifier = AnnouncementIdentifier(rawAnnouncementIdentifier)
+            let announcementContent = AnnouncementContentRepresentation(identifier: announcementIdentifier)
+            recipient.receive(announcementContent)
         }
     }
     
@@ -35,17 +61,42 @@ class NotificationContentRepresentationTests: XCTestCase {
     
     func testEventNotification() {
         let eventIdentifierString = String.random
-        let userInfo: [AnyHashable: Any] = [
+        assertUserInfo([
             ApplicationNotificationKey.notificationContentKind: ApplicationNotificationContentKind.event,
             ApplicationNotificationKey.notificationContentIdentifier: eventIdentifierString
-        ]
-        
+        ], isResolvedIntoContent: EventContentRepresentation(identifier: EventIdentifier(eventIdentifierString)))
+    }
+    
+    func testMessageNotification() {
+        let messageIdentifierString = String.random
+        assertUserInfo(
+            ["message_id": messageIdentifierString],
+            isResolvedIntoContent: MessageContentRepresentation(
+                identifier: MessageIdentifier(messageIdentifierString)
+            )
+        )
+    }
+    
+    func testAnnouncementNotification() {
+        let announcementIdentitiferString = String.random
+        assertUserInfo(
+            ["announcement_id": announcementIdentitiferString],
+            isResolvedIntoContent: AnnouncementContentRepresentation(
+                identifier: AnnouncementIdentifier(announcementIdentitiferString)
+            )
+        )
+    }
+    
+    private func assertUserInfo<Content>(
+        _ userInfo: [AnyHashable: Any],
+        isResolvedIntoContent expected: Content,
+        _ line: UInt = #line
+    ) where Content: ContentRepresentation {
         let contentRepresentation = NotificationContentRepresentation(userInfo: userInfo)
         let recipient = CapturingContentRepresentationRecipient()
         contentRepresentation.describe(to: recipient)
         
-        let expected = EventContentRepresentation(identifier: EventIdentifier(eventIdentifierString))
-        XCTAssertEqual(expected.eraseToAnyContentRepresentation(), recipient.erasedRoutedContent)
+        XCTAssertEqual(expected.eraseToAnyContentRepresentation(), recipient.erasedRoutedContent, line: line)
     }
 
 }
