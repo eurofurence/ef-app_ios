64d5ff11f30a339a3edc976719f1414a55a1f2ee
ShezHsky <shezhsky@icloud.com>
2020-05-07 21:54:19 +0100

    Consolidate setup and test case

--- EurofurenceTests/Presenter Tests/Messages/WhenSelectingMessage_MessagesPresenterShould.swift
+++ EurofurenceTests/Presenter Tests/Messages/WhenSelectingMessage_MessagesPresenterShould.swift
@@ -1,23 +1,16 @@
-@testable import Eurofurence
+import Eurofurence
 import EurofurenceModel
 import EurofurenceModelTestDoubles
 import XCTest
 
 class WhenViewingMessageWhileLoggedInWithLocalMessage: XCTestCase {
 
-    var context: MessagesPresenterTestContext!
-    var localMessage: StubMessage!
-
-    override func setUp() {
-        super.setUp()
-
-        localMessage = .random
-        context = MessagesPresenterTestContext.makeTestCaseForUserWithMessages([localMessage])
+    func testTellTheDelegateToShowTheMessage() {
+        let localMessage = StubMessage.random
+        let context = MessagesPresenterTestContext.makeTestCaseForUserWithMessages([localMessage])
         context.scene.delegate?.messagesSceneReady()
         context.privateMessagesService.succeedLastRefresh(messages: [localMessage])
-    }
-
-    func testSelectingMessageTellsDelegateToShowTheMessage() {
+        
         context.scene.tapMessage(at: 0)
         XCTAssertEqual(localMessage.identifier, context.delegate.messageToShow)
     }
