58397ce3b594dabaff85da9075813196c670b44f
ShezHsky <shezhsky@icloud.com>
2020-05-10 22:25:26 +0100

    Consolidate announcement detail vm tests

--- EurofurenceTests/Application/Components/Announcement Detail/View Model Tests/DefaultAnnouncementDetailViewModelFactoryShould.swift
+++ EurofurenceTests/Application/Components/Announcement Detail/View Model Tests/DefaultAnnouncementDetailViewModelFactoryShould.swift
@@ -4,29 +4,16 @@ import XCTest
 
 class DefaultAnnouncementDetailViewModelFactoryShould: XCTestCase {
 
-    var context: AnnouncementDetailViewModelFactoryTestBuilder.Context!
-
-    override func setUp() {
-        super.setUp()
-        
+    func testProduceViewModelUsingAnnouncementAttributes() {
         let imagePNGData = "Strike a poke".data(using: .utf8).unsafelyUnwrapped
-        context = AnnouncementDetailViewModelFactoryTestBuilder()
+        let context = AnnouncementDetailViewModelFactoryTestBuilder()
             .with(imagePNGData: imagePNGData)
             .build()
-    }
-
-    func testProduceViewModelUsingAnnouncementTitleAsHeading() {
+        
         let viewModel = context.makeViewModel()
+        
         XCTAssertEqual(context.announcement.title, viewModel?.heading)
-    }
-
-    func testProduceViewModelContentsUsingMarkdownRenderer() {
-        let viewModel = context.makeViewModel()
         XCTAssertEqual(context.markdownRenderer.stubbedContents(for: context.announcement.content), viewModel?.contents)
-    }
-
-    func testProduceViewModelWithAnnouncementImage() {
-        let viewModel = context.makeViewModel()
         XCTAssertEqual(context.announcement.imagePNGData, viewModel?.imagePNGData)
     }
     
