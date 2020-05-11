251bb525e21554d2fa350f75af583a92d740d6ea
ShezHsky <shezhsky@icloud.com>
2020-05-11 10:01:07 +0100

    Consolidate similar tests

--- EurofurenceTests/Application/Components/Dealers/Presenter Tests/Categories/WhenBindingInactiveCategory_DealersSceneShould.swift
+++ EurofurenceTests/Application/Components/Dealers/Presenter Tests/Categories/WhenBindingInactiveCategory_DealersSceneShould.swift
@@ -4,15 +4,35 @@ import XCTest
 class WhenBindingInactiveCategory_DealersSceneShould: XCTestCase {
 
     func testHideCategoryActiveIndicator() {
+        let inactiveCategory = FakeDealerCategoryViewModel(title: "A")
+        inactiveCategory.enterInactiveState()
+        
+        assertCategoryActiveVisibility(.hidden, forCategory: inactiveCategory)
+    }
+    
+    func testShowCategoryActiveIndicator() {
         let activeCategory = FakeDealerCategoryViewModel(title: "A")
-        activeCategory.enterInactiveState()
-        let categoriesViewModel = FakeDealerCategoriesViewModel(categories: [activeCategory])
+        activeCategory.enterActiveState()
+        
+        assertCategoryActiveVisibility(.visible, forCategory: activeCategory)
+    }
+    
+    private func assertCategoryActiveVisibility(
+        _ expected: VisibilityState,
+        forCategory category: DealerCategoryViewModel,
+        _ line: UInt = #line
+    ) {
+        let categoriesViewModel = FakeDealerCategoriesViewModel(categories: [category])
         let viewModelFactory = FakeDealersViewModelFactory(viewModel: CapturingDealersViewModel.random, categoriesViewModel: categoriesViewModel)
         let context = DealersPresenterTestBuilder().with(viewModelFactory).build()
         context.simulateSceneDidLoad()
         context.simulateSceneDidRevealCategoryFilters()
         
-        XCTAssertEqual(.hidden, context.scene.filtersScene.visibilityForCategoryActiveIndicator(at: 0))
+        XCTAssertEqual(
+            expected,
+            context.scene.filtersScene.visibilityForCategoryActiveIndicator(at: 0),
+            line: line
+        )
     }
 
 }
