1230408c97bccbac3e22876fadb1f507095bdb8b
ShezHsky <shezhsky@icloud.com>
2020-07-20 16:42:24 +0100

    Patch up test

--- EurofurenceTests/Application/Components/Principal Content/PrincipalContentAggregatorTests.swift
+++ EurofurenceTests/Application/Components/Principal Content/PrincipalContentAggregatorTests.swift
@@ -4,16 +4,15 @@ import XCTest
 class PrincipalContentAggregatorTests: XCTestCase {
     
     func testWrapsInputControllersInsideNavigationAndTabControllers() throws {
-        let (first, second) = (StubContentControllerFactory(), StubContentControllerFactory())
-        let aggregator = PrincipalContentAggregator(applicationModuleFactories: [first, second])
+        let contentControllerFactory = StubContentControllerFactory()
+        let aggregator = PrincipalContentAggregator(applicationModuleFactories: [contentControllerFactory])
         let principalModule = aggregator.makePrincipalContentModule()
         
         let tabController = try XCTUnwrap(principalModule as? UITabBarController)
-        let firstNav = try XCTUnwrap(tabController.viewControllers?[0] as? UINavigationController)
-        let secondNav = try XCTUnwrap(tabController.viewControllers?[1] as? UINavigationController)
+        let splitViewController = try XCTUnwrap(tabController.viewControllers?[0] as? UISplitViewController)
+        let navigationController = try XCTUnwrap(splitViewController.viewControllers[0] as? UINavigationController)
         
-        XCTAssertEqual(firstNav.viewControllers.first, first.stubInterface)
-        XCTAssertEqual(secondNav.viewControllers.first, second.stubInterface)
+        XCTAssertEqual(navigationController.viewControllers.first, contentControllerFactory.stubInterface)
     }
 
 }
