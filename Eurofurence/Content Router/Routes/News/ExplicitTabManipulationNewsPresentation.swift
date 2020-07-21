1be7f056d33cacbb516a09fa83f4b3cb9afbbdfe
ShezHsky <shezhsky@icloud.com>
2020-07-21 20:29:35 +0100

    Fix return to news navigation

--- Eurofurence/Content Router/Routes/News/ExplicitTabManipulationNewsPresentation.swift
+++ Eurofurence/Content Router/Routes/News/ExplicitTabManipulationNewsPresentation.swift
@@ -8,13 +8,16 @@ struct ExplicitTabManipulationNewsPresentation: NewsPresentation {
         guard let tabBarController = window.rootViewController as? UITabBarController else { return }
         guard let newsNavigationController = tabBarController
             .viewControllers?
-            .compactMap({ $0 as? UINavigationController })
+            .compactMap({ $0 as? UISplitViewController })
+            .compactMap({ $0.viewControllers.first as? UINavigationController })
             .first(where: { $0.viewControllers.contains(where: { $0 is NewsViewController }) }) else {
                 return
         }
         
         guard let newsControllerIndex = tabBarController
             .viewControllers?
+            .compactMap({ $0 as? UISplitViewController })
+            .compactMap({ $0.viewControllers.first as? UINavigationController })
             .firstIndex(of: newsNavigationController) else { return }
         
         tabBarController.selectedIndex = newsControllerIndex
