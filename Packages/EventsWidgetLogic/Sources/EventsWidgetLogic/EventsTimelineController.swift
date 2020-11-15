c71e7f4a07d5f3b149f6bb95990879d067fb9cbe
ShezHsky <shezhsky@icloud.com>
2020-11-15 11:38:10 +0000

    Extract methods

--- Packages/EventsWidgetLogic/Sources/EventsWidgetLogic/EventsTimelineController.swift
+++ Packages/EventsWidgetLogic/Sources/EventsWidgetLogic/EventsTimelineController.swift
@@ -10,30 +10,38 @@ public struct EventsTimelineController {
     
     public func makeEntries(completionHandler: @escaping ([EventTimelineEntry]) -> Void) {
         repository.loadEvents { (events) in
-            let distinctStartTimes = Set(events.map(\.startTime)).sorted()
-            
-            var eventClusters = [[Event]]()
-            for startTime in distinctStartTimes {
-                let eventsOnOrAfterTime = events.filter({ $0.startTime >= startTime })
-                eventClusters.append(eventsOnOrAfterTime)
-            }
-            
-            var entries = [EventTimelineEntry]()
-            for cluster in eventClusters {
-                guard let firstInCluster = cluster.first else { continue }
-                let entry = makeTimelineEntry(events: cluster, relevantDate: firstInCluster.startTime)
-                entries.append(entry)
-            }
+            let eventClusters = clusterEventsByStartTime(events)
+            let entries = eventClusters.map(makeTimelineEntry(cluster:))
             
             completionHandler(entries)
         }
     }
     
-    private func makeTimelineEntry(events: [Event], relevantDate: Date) -> EventTimelineEntry {
-        let viewModels = events.map(EventViewModel.init)
-        let entry = EventTimelineEntry(date: relevantDate, events: viewModels, additionalEventsCount: 0)
+    private func clusterEventsByStartTime(_ events: [Event]) -> [EventCluster] {
+        let distinctStartTimes = Set(events.map(\.startTime)).sorted()
+        
+        var eventClusters = [EventCluster]()
+        for startTime in distinctStartTimes {
+            let eventsOnOrAfterTime = events.filter({ $0.startTime >= startTime })
+            let cluster = EventCluster(clusterStartTime: startTime, events: eventsOnOrAfterTime)
+            eventClusters.append(cluster)
+        }
+        
+        return eventClusters
+    }
+    
+    private func makeTimelineEntry(cluster: EventCluster) -> EventTimelineEntry {
+        let viewModels = cluster.events.map(EventViewModel.init)
+        let entry = EventTimelineEntry(date: cluster.clusterStartTime, events: viewModels, additionalEventsCount: 0)
         
         return entry
+    }
+    
+    private struct EventCluster {
+        
+        var clusterStartTime: Date
+        var events: [Event]
+        
     }
     
 }
