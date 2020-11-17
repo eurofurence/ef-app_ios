f23bc9338aec7edb718c63113723cb27f7a0d337
ShezHsky <shezhsky@icloud.com>
2020-11-17 12:45:20 +0000

    Inline field

--- EventsWidget/EventsTimelineProvider.swift
+++ EventsWidget/EventsTimelineProvider.swift
@@ -3,36 +3,34 @@ import WidgetKit
 
 struct EventsTimelineProvider: IntentTimelineProvider {
     
-    private let sampleEvents: [EventViewModel] = [
-        EventViewModel(
-            id: UUID().uuidString,
-            title: "Trans Meet-Up",
-            location: "Nizza",
-            formattedStartTime: "13:00",
-            formattedEndTime: "14:30"
-        ),
-        
-        EventViewModel(
-            id: UUID().uuidString,
-            title: "Dealer's Den",
-            location: "Dealer's Den - Convention Center Foyer 3",
-            formattedStartTime: "13:30",
-            formattedEndTime: "15:00"
-        ),
-        
-        EventViewModel(
-            id: UUID().uuidString,
-            title: "Funny Animals and Amerimanga in Sonic the Hedgehog Archie Series",
-            location: "Nizza",
-            formattedStartTime: "17:30",
-            formattedEndTime: "18:30"
-        )
-    ]
-    
     func placeholder(in context: Context) -> EventTimelineEntry {
         EventTimelineEntry(
             date: Date(),
-            events: sampleEvents,
+            events: [
+                EventViewModel(
+                    id: UUID().uuidString,
+                    title: "Trans Meet-Up",
+                    location: "Nizza",
+                    formattedStartTime: "13:00",
+                    formattedEndTime: "14:30"
+                ),
+                
+                EventViewModel(
+                    id: UUID().uuidString,
+                    title: "Dealer's Den",
+                    location: "Dealer's Den - Convention Center Foyer 3",
+                    formattedStartTime: "13:30",
+                    formattedEndTime: "15:00"
+                ),
+                
+                EventViewModel(
+                    id: UUID().uuidString,
+                    title: "Funny Animals and Amerimanga in Sonic the Hedgehog Archie Series",
+                    location: "Nizza",
+                    formattedStartTime: "17:30",
+                    formattedEndTime: "18:30"
+                )
+            ],
             additionalEventsCount: 1
         )
     }
