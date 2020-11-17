5d89fa9dc1481ed82e37fef5895e50b24885cabc
ShezHsky <shezhsky@icloud.com>
2020-11-17 08:51:01 +0000

    Update event count rules per widget family

--- Packages/EventsWidgetLogic/Sources/EventsWidgetLogic/EventWidgetContext.swift
+++ Packages/EventsWidgetLogic/Sources/EventsWidgetLogic/EventWidgetContext.swift
@@ -1,14 +1,31 @@
 public struct EventWidgetContext {
     
     public enum AccessibilityCategory {
+        
         case standard
         case large
+        
     }
     
     public enum WidgetSize {
+        
         case small
         case medium
         case large
+        
+        func numberOfWidgets(in accessibilityCategory: AccessibilityCategory) -> Int {
+            switch self {
+            case .small:
+                return accessibilityCategory == .standard ? 2 : 1
+                
+            case .medium:
+                return accessibilityCategory == .standard ? 3 : 2
+                
+            default:
+                return accessibilityCategory == .standard ? 5 : 3
+            }
+        }
+        
     }
     
     let accessibilityCategory: AccessibilityCategory
@@ -20,7 +37,7 @@ public struct EventWidgetContext {
     }
     
     public var recommendedNumberOfEvents: Int {
-        accessibilityCategory == .standard ? 5 : 3
+        widgetSize.numberOfWidgets(in: accessibilityCategory)
     }
     
 }
