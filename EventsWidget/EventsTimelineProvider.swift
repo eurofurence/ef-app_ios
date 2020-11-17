d0d71eb504ade000f63c789c80ebc7ff14573215
ShezHsky <shezhsky@icloud.com>
2020-11-17 13:13:57 +0000

    Update placeholder template to be more realistic

--- EventsWidget/EventsTimelineProvider.swift
+++ EventsWidget/EventsTimelineProvider.swift
@@ -4,33 +4,54 @@ import WidgetKit
 struct EventsTimelineProvider: IntentTimelineProvider {
     
     func placeholder(in context: Context) -> EventTimelineEntry {
-        EventTimelineEntry(
+        let widgetContext = EventWidgetContext(timelineContext: context)
+        
+        let sampleEvents = [
+            EventViewModel(
+                id: UUID().uuidString,
+                title: "Trans Meet-Up",
+                location: "Nizza",
+                formattedStartTime: "13:00",
+                formattedEndTime: "14:30"
+            ),
+            
+            EventViewModel(
+                id: UUID().uuidString,
+                title: "Dealer's Den",
+                location: "Dealer's Den - Convention Center Foyer 3",
+                formattedStartTime: "13:30",
+                formattedEndTime: "15:00"
+            ),
+            
+            EventViewModel(
+                id: UUID().uuidString,
+                title: "Funny Animals and Amerimanga in Sonic the Hedgehog Archie Series",
+                location: "Nizza",
+                formattedStartTime: "17:30",
+                formattedEndTime: "18:30"
+            ),
+            
+            EventViewModel(
+                id: UUID().uuidString,
+                title: "Fursuit Photoshoot Registration",
+                location: "Fursuit Photoshoot Registration - Estrel Hall B",
+                formattedStartTime: "19:00",
+                formattedEndTime: "20:30"
+            ),
+            
+            EventViewModel(
+                id: UUID().uuidString,
+                title: "International Snack Exchange",
+                location: "ECC Room 3",
+                formattedStartTime: "22:00",
+                formattedEndTime: "00:30"
+            )
+        ]
+        let placeholderEvents = sampleEvents.prefix(widgetContext.recommendedNumberOfEvents)
+        
+        return EventTimelineEntry(
             date: Date(),
-            events: [
-                EventViewModel(
-                    id: UUID().uuidString,
-                    title: "Trans Meet-Up",
-                    location: "Nizza",
-                    formattedStartTime: "13:00",
-                    formattedEndTime: "14:30"
-                ),
-                
-                EventViewModel(
-                    id: UUID().uuidString,
-                    title: "Dealer's Den",
-                    location: "Dealer's Den - Convention Center Foyer 3",
-                    formattedStartTime: "13:30",
-                    formattedEndTime: "15:00"
-                ),
-                
-                EventViewModel(
-                    id: UUID().uuidString,
-                    title: "Funny Animals and Amerimanga in Sonic the Hedgehog Archie Series",
-                    location: "Nizza",
-                    formattedStartTime: "17:30",
-                    formattedEndTime: "18:30"
-                )
-            ],
+            events: Array(placeholderEvents),
             additionalEventsCount: 1
         )
     }
