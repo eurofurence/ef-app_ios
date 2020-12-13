d8a1cd14f0e069bbea059ac9e1a8c212f7fa2065
ShezHsky <shezhsky@icloud.com>
2020-12-13 11:35:47 +0000

    Extract methods for creating timeline

--- Packages/EventsWidgetLogic/Sources/EventsWidgetLogic/Adapting Events/ResolveTimelineEntriesTask.swift
+++ Packages/EventsWidgetLogic/Sources/EventsWidgetLogic/Adapting Events/ResolveTimelineEntriesTask.swift
@@ -28,48 +28,56 @@ struct ResolveTimelineEntriesTask {
     }
     
     private func makeTimeline(from clusters: [EventCluster]) -> EventsTimeline {
-        let snapshotEntry: EventTimelineEntry
-        var timelineEntries = [EventTimelineEntry]()
-        
         if let first = clusters.first {
-            let remainingEntries: [EventTimelineEntry]
-            if first.clusterStartTime > timelineStartDate {
-                snapshotEntry = EventTimelineEntry(
-                    date: timelineStartDate,
-                    content: .empty,
-                    context: .init(category: eventCategory, isFavouritesOnly: isFavouritesOnly)
-                )
-                
-                remainingEntries = clusters.map(makeTimelineEntry(from:))
-            } else {
-                snapshotEntry = makeTimelineEntry(from: first)
-                remainingEntries = clusters.suffix(from: 1).map(makeTimelineEntry(from:))
-            }
-            
-            timelineEntries.append(snapshotEntry)
-            timelineEntries.append(contentsOf: remainingEntries)
-            
-            let lastCluster = clusters.last ?? first
-            
-            let endOfSchedule = EventTimelineEntry(
-                date: lastCluster.lastEventTimeInCluster,
+            return makeEventsTimeline(firstCluster: first, allClusters: clusters)
+        } else {
+            return makeEmptyTimeline()
+        }
+    }
+    
+    private func makeEventsTimeline(firstCluster: EventCluster, allClusters: [EventCluster]) -> EventsTimeline {
+        let snapshotEntry: EventTimelineEntry
+        let remainingEntries: [EventTimelineEntry]
+        
+        if firstCluster.clusterStartTime > timelineStartDate {
+            snapshotEntry = EventTimelineEntry(
+                date: timelineStartDate,
                 content: .empty,
                 context: .init(category: eventCategory, isFavouritesOnly: isFavouritesOnly)
             )
             
-            timelineEntries.append(endOfSchedule)
+            remainingEntries = allClusters.map(makeTimelineEntry(from:))
         } else {
-            snapshotEntry = EventTimelineEntry(
-                date: timelineStartDate,
-                content: .empty,
-                context: EventTimelineEntry.Context(category: eventCategory, isFavouritesOnly: isFavouritesOnly)
-            )
-            
-            timelineEntries = [snapshotEntry]
+            snapshotEntry = makeTimelineEntry(from: firstCluster)
+            remainingEntries = allClusters.suffix(from: 1).map(makeTimelineEntry(from:))
         }
+        
+        var timelineEntries = [EventTimelineEntry]()
+        timelineEntries.append(snapshotEntry)
+        timelineEntries.append(contentsOf: remainingEntries)
+        
+        let lastCluster = allClusters.last ?? firstCluster
+        
+        let endOfSchedule = EventTimelineEntry(
+            date: lastCluster.lastEventTimeInCluster,
+            content: .empty,
+            context: .init(category: eventCategory, isFavouritesOnly: isFavouritesOnly)
+        )
+        
+        timelineEntries.append(endOfSchedule)
         
         let timeline = EventsTimeline(snapshot: snapshotEntry, entries: timelineEntries)
         return timeline
+    }
+    
+    private func makeEmptyTimeline() -> EventsTimeline {
+        let snapshotEntry = EventTimelineEntry(
+            date: timelineStartDate,
+            content: .empty,
+            context: EventTimelineEntry.Context(category: eventCategory, isFavouritesOnly: isFavouritesOnly)
+        )
+        
+        return EventsTimeline(snapshot: snapshotEntry, entries: [snapshotEntry])
     }
     
     private func makeTimelineEntry(from cluster: EventCluster) -> EventTimelineEntry {
