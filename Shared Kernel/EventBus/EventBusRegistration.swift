0d28903baf0bec5f3c1ccdcea926d971ca550c42
ShezHsky <shezhsky@icloud.com>
2021-03-10 10:11:37 +0000

    Drop unused event bus behaviour

--- Shared Kernel/EventBus/EventBusRegistration.swift
+++ Shared Kernel/EventBus/EventBusRegistration.swift
@@ -1,7 +1,6 @@
 protocol EventBusRegistration {
 
     func supports<T>(_ event: T) -> Bool
-    func represents<Consumer: EventConsumer>(consumer: Consumer) -> Bool
     func handle(event: Any)
 
 }
