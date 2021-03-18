de556d02e0b605ba669f7cacecbf3bde2e6d33a1
ShezHsky <shezhsky@icloud.com>
2021-03-18 19:01:01 +0000

    Hook up UI into app clip

--- DealersAppClip/DealersSceneDelegate.swift
+++ DealersAppClip/DealersSceneDelegate.swift
@@ -1,3 +1,8 @@
+import ComponentBase
+import DealerComponent
+import DealersComponent
+import EurofurenceApplicationSession
+import EurofurenceModel
 import UIKit
 
 class DealersSceneDelegate: UIResponder, UIWindowSceneDelegate {
@@ -11,14 +16,69 @@ class DealersSceneDelegate: UIResponder, UIWindowSceneDelegate {
     ) {
         guard let windowScene = scene as? UIWindowScene else { return }
         
-        let blank = UIViewController()
-        blank.view.backgroundColor = .systemRed
-        
         let window = UIWindow(windowScene: windowScene)
-        window.rootViewController = blank
-        window.makeKeyAndVisible()
+        let rootViewController = BrandedSplitViewController()
+        window.rootViewController = rootViewController
+        
+        let session = EurofurenceSessionBuilder.buildingForEurofurenceApplication().build()
+        let services = session.services
+        
+        struct DoNotDonateDealerInteraction: DealerInteractionRecorder {
+            
+            func makeInteraction(for dealer: DealerIdentifier) -> Interaction? {
+                nil
+            }
+            
+        }
+        
+        let dealersViewModelFactory = DefaultDealersViewModelFactory(
+            dealersService: services.dealers,
+            refreshService: services.refresh
+        )
+        
+        let dealersComponentFactory = DealersComponentBuilder(
+            dealersViewModelFactory: dealersViewModelFactory
+        ).build()
+        
+        let shareService = ActivityShareService(window: window)
+        let dealerDetailViewModelFactory = DefaultDealerDetailViewModelFactory(
+            dealersService: services.dealers,
+            shareService: shareService
+        )
+
+        let dealerDetailModuleProviding = DealerDetailComponentBuilder(
+            dealerDetailViewModelFactory: dealerDetailViewModelFactory,
+            dealerInteractionRecorder: DoNotDonateDealerInteraction()
+        ).build()
+        
+        let showDealerInDetailPane = ShowDealerInDetailPane(
+            splitViewController: rootViewController,
+            dealerDetailModuleProviding: dealerDetailModuleProviding
+        )
+        
+        let dealersModule = dealersComponentFactory.makeDealersComponent(showDealerInDetailPane)
+        let dealersNavigationController = BrandedNavigationController(rootViewController: dealersModule)
+        let placeholderViewController = NoContentPlaceholderViewController.fromStoryboard()
+        let placeholderNavigation = BrandedNavigationController(rootViewController: placeholderViewController)
+        rootViewController.viewControllers = [dealersNavigationController, placeholderNavigation]
         
         self.window = window
+        window.makeKeyAndVisible()
+    }
+    
+    struct ShowDealerInDetailPane: DealersComponentDelegate {
+        
+        var splitViewController: UISplitViewController
+        var dealerDetailModuleProviding: DealerDetailComponentFactory
+        
+        func dealersModuleDidSelectDealer(identifier: DealerIdentifier) {
+            let dealerDetailComponent = dealerDetailModuleProviding.makeDealerDetailComponent(for: identifier)
+            splitViewController.showDetailViewController(
+                dealerDetailComponent,
+                sender: DetailPresentationContext.replace
+            )
+        }
+        
     }
 
 }
