dfa0dc0dd2d9cc3527dc161a8aec1a437fb33000
ShezHsky <shezhsky@icloud.com>
2021-03-20 23:30:56 +0000

    Add UI test target for app clip

---
+++ Eurofurence Clip UITests/Eurofurence_Clip_UITests.swift
@@ -0,0 +1,18 @@
+import XCTest
+
+class Eurofurence_Clip_UITests: XCTestCase {
+
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+        continueAfterFailure = false
+    }
+
+    func testLaunchPerformance() throws {
+        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
+            measure(metrics: [XCTApplicationLaunchMetric()]) {
+                XCUIApplication().launch()
+            }
+        }
+    }
+    
+}
