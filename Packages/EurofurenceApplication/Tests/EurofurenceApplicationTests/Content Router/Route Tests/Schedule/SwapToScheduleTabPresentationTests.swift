eee78a1e11919e617307f413a6f9be318c14eb25
ShezHsky <shezhsky@icloud.com>
2021-05-27 12:52:04 +0100

    Add presentation implementation for schedule navigation

---
+++ Packages/EurofurenceApplication/Tests/EurofurenceApplicationTests/Content Router/Route Tests/Schedule/SwapToScheduleTabPresentationTests.swift
@@ -0,0 +1,18 @@
+import EventsJourney
+import EurofurenceApplication
+import XCTest
+
+class SwapToScheduleTabPresentationTests: XCTestCase {
+    
+    func testInvokesTabNavigation() {
+        let tabNavigation = CapturingTabNavigator()
+        let presentation = SwapToScheduleTabPresentation(tabNavigator: tabNavigation)
+        
+        XCTAssertFalse(tabNavigation.didMoveToTab)
+        
+        presentation.showSchedule()
+        
+        XCTAssertTrue(tabNavigation.didMoveToTab)
+    }
+    
+}
