7764d8a228123532d3aecc7be0d0dcda2fc1ba03
ShezHsky <shezhsky@icloud.com>
2021-06-15 20:46:35 +0100

    Add container controller implementation

---
+++ Packages/EurofurenceClipLogic/Sources/EurofurenceClipLogic/Routing/Root Wireframe/RootContainerViewController.swift
@@ -0,0 +1,39 @@
+import UIKit
+
+public class RootContainerViewController: UIViewController {
+    
+    public init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .clear
+    }
+    
+}
+
+
+// MARK: - RootContainerViewController + ReplaceRootWireframe
+
+extension RootContainerViewController: ReplaceRootWireframe {
+    
+    public func replaceRoot(with newRoot: UIViewController) {
+        if let existingRoot = children.first {
+            existingRoot.willMove(toParent: nil)
+            existingRoot.view.removeFromSuperview()
+            existingRoot.removeFromParent()
+            existingRoot.didMove(toParent: nil)
+        }
+        
+        newRoot.willMove(toParent: self)
+        view.addSubview(newRoot.view)
+        addChild(newRoot)
+        newRoot.didMove(toParent: self)
+    }
+    
+}
