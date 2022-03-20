9a647340cb518db6eb825b8104f0c886d2c3bb8e
ShezHsky <shezhsky@icloud.com>
2022-03-20 17:56:41 +0000

    Remove unused override causing a warning

--- Packages/EurofurenceComponents/Sources/ScheduleComponent/View/UIKit/ScheduleSearchTableViewController.swift
+++ Packages/EurofurenceComponents/Sources/ScheduleComponent/View/UIKit/ScheduleSearchTableViewController.swift
@@ -58,22 +58,6 @@ class ScheduleSearchTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         onDidSelectSearchResultAtIndexPath?(indexPath)
     }
-
-#if !targetEnvironment(macCatalyst)
-    override func tableView(
-        _ tableView: UITableView,
-        editActionsForRowAt indexPath: IndexPath
-    ) -> [UITableViewRowAction]? {
-        guard let actions = binder?.eventActionsForComponent(at: indexPath) else { return nil }
-        guard let sender = tableView.cellForRow(at: indexPath) else { return nil }
-        
-        return actions.map { (action) in
-            UITableViewRowAction(style: .normal, title: action.title, handler: { (_, _) in
-                action.run(sender)
-            })
-        }
-    }
-#endif
     
     override func tableView(
         _ tableView: UITableView,
