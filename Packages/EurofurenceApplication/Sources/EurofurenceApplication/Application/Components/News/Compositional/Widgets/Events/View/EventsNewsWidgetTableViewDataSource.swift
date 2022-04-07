e05d22c3639c73e5d250b720920bbb466c9aaf5d
ShezHsky <shezhsky@icloud.com>
2022-04-07 07:56:47 +0100

    Decompose large method

--- Packages/EurofurenceApplication/Sources/EurofurenceApplication/Application/Components/News/Compositional/Widgets/Events/View/EventsNewsWidgetTableViewDataSource.swift
+++ Packages/EurofurenceApplication/Sources/EurofurenceApplication/Application/Components/News/Compositional/Widgets/Events/View/EventsNewsWidgetTableViewDataSource.swift
@@ -45,72 +45,118 @@ public class EventsNewsWidgetTableViewDataSource<T>: NSObject, TableViewMediator
     
     public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeue(EventTableViewCell.self, for: indexPath)
-        cell.subscriptions.removeAll()
-        
         let event = viewModel.event(at: indexPath.row)
-        cell.setEventName(event.name)
-        cell.setLocation(event.location)
-        cell.setEventStartTime(event.startTime)
-        cell.setEventEndTime(event.endTime)
-        
-        event
-            .publisher(for: \.isFavourite)
-            .sink { [weak cell] (isFavourite) in
-                if isFavourite {
-                    cell?.showFavouriteEventIndicator()
-                } else {
-                    cell?.hideFavouriteEventIndicator()
-                }
-            }
-            .store(in: &cell.subscriptions)
-        
-        if event.isSponsorOnly {
-            cell.showSponsorEventIndicator()
-        } else {
-            cell.hideSponsorEventIndicator()
-        }
-        
-        if event.isSuperSponsorOnly {
-            cell.showSuperSponsorOnlyEventIndicator()
-        } else {
-            cell.hideSuperSponsorOnlyEventIndicator()
-        }
-        
-        if event.isArtShow {
-            cell.showArtShowEventIndicator()
-        } else {
-            cell.hideArtShowEventIndicator()
-        }
-        
-        if event.isDealersDen {
-            cell.showDealersDenEventIndicator()
-        } else {
-            cell.hideDealersDenEventIndicator()
-        }
-        
-        if event.isMainStage {
-            cell.showMainStageEventIndicator()
-        } else {
-            cell.hideMainStageEventIndicator()
-        }
-        
-        if event.isPhotoshoot {
-            cell.showPhotoshootStageEventIndicator()
-        } else {
-            cell.hidePhotoshootStageEventIndicator()
-        }
-        
-        if event.isKageEvent {
-            cell.showKageEventIndicator()
-        } else {
-            cell.hideKageEventIndicator()
-        }
+        ViewModelBinder(cell: cell, event: event).bind()
         
         return cell
     }
     
     public func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         viewModel.eventSelected(at: indexPath.row)
+    }
+    
+    private struct ViewModelBinder {
+        
+        let cell: EventTableViewCell
+        let event: T.Event
+        
+        func bind() {
+            removeExistingSubscriptions()
+            bindPrimaryAttributes()
+            bindEventTypeIndicators()
+            bindFavouriteStatus()
+        }
+        
+        private func removeExistingSubscriptions() {
+            cell.subscriptions.removeAll()
+        }
+        
+        private func bindPrimaryAttributes() {
+            cell.setEventName(event.name)
+            cell.setLocation(event.location)
+            cell.setEventStartTime(event.startTime)
+            cell.setEventEndTime(event.endTime)
+        }
+        
+        private func bindEventTypeIndicators() {
+            bindSponsorOnlyIndicator()
+            bindSuperSponsorOnlyIndicator()
+            bindArtShowIndicator()
+            bindDealersDenIndicator()
+            bindMainStageIndicator()
+            bindPhotoshootIndicator()
+            bindKageEventIndicator()
+        }
+        
+        private func bindSponsorOnlyIndicator() {
+            if event.isSponsorOnly {
+                cell.showSponsorEventIndicator()
+            } else {
+                cell.hideSponsorEventIndicator()
+            }
+        }
+        
+        private func bindSuperSponsorOnlyIndicator() {
+            if event.isSuperSponsorOnly {
+                cell.showSuperSponsorOnlyEventIndicator()
+            } else {
+                cell.hideSuperSponsorOnlyEventIndicator()
+            }
+        }
+        
+        private func bindArtShowIndicator() {
+            if event.isArtShow {
+                cell.showArtShowEventIndicator()
+            } else {
+                cell.hideArtShowEventIndicator()
+            }
+        }
+        
+        private func bindDealersDenIndicator() {
+            if event.isDealersDen {
+                cell.showDealersDenEventIndicator()
+            } else {
+                cell.hideDealersDenEventIndicator()
+            }
+        }
+        
+        private func bindMainStageIndicator() {
+            if event.isMainStage {
+                cell.showMainStageEventIndicator()
+            } else {
+                cell.hideMainStageEventIndicator()
+            }
+        }
+        
+        private func bindPhotoshootIndicator() {
+            if event.isPhotoshoot {
+                cell.showPhotoshootStageEventIndicator()
+            } else {
+                cell.hidePhotoshootStageEventIndicator()
+            }
+        }
+        
+        private func bindKageEventIndicator() {
+            if event.isKageEvent {
+                cell.showKageEventIndicator()
+            } else {
+                cell.hideKageEventIndicator()
+            }
+        }
+        
+        private func bindFavouriteStatus() {
+            event
+                .publisher(for: \.isFavourite)
+                .sink { [weak cell] (isFavourite) in
+                    if isFavourite {
+                        cell?.showFavouriteEventIndicator()
+                    } else {
+                        cell?.hideFavouriteEventIndicator()
+                    }
+                }
+                .store(in: &cell.subscriptions)
+        }
+        
     }
     
 }
