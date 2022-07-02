eb999717133ae382924f6eee3153f3809dd64b39
ShezHsky <shezhsky@icloud.com>
2022-07-02 19:12:50 +0100

    Increase scrolling speed in test

--- EurofurenceUITests/UI Automation/AutomationController.swift
+++ EurofurenceUITests/UI Automation/AutomationController.swift
@@ -113,7 +113,7 @@ extension AutomationController {
         while textElement.isHittable == false {
             let now = Date()
             if now < upperTimeLimit {
-                table.swipeUp(velocity: 100)
+                table.swipeUp(velocity: 400)
             } else {
                 throw TimedOutFindingText(text: text)
             }
