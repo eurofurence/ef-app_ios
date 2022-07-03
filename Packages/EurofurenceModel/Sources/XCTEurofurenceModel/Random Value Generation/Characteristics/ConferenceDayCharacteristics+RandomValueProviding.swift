2f4cf445529e22e983b90e84e44fd94fa197ea15
ShezHsky <shezhsky@icloud.com>
2022-07-03 12:00:24 +0100

    Fix event favourite state changes reloading schedule
    Also removes duplicated day filtering concept

--- Packages/EurofurenceModel/Sources/XCTEurofurenceModel/Random Value Generation/Characteristics/ConferenceDayCharacteristics+RandomValueProviding.swift
+++ Packages/EurofurenceModel/Sources/XCTEurofurenceModel/Random Value Generation/Characteristics/ConferenceDayCharacteristics+RandomValueProviding.swift
@@ -5,7 +5,16 @@ import TestUtilities
 extension ConferenceDayCharacteristics: RandomValueProviding {
     
     public static var random: ConferenceDayCharacteristics {
-        return ConferenceDayCharacteristics(identifier: .random, date: .random)
+        let randomDate = Date.random
+        var components = Calendar.current.dateComponents(in: .current, from: randomDate)
+        components.hour = 0
+        components.minute = 0
+        components.second = 0
+        components.nanosecond = 0
+        
+        let normalizedDate = components.date.unsafelyUnwrapped
+        
+        return ConferenceDayCharacteristics(identifier: .random, date: normalizedDate)
     }
     
 }
