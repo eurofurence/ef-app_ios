99e856ca881aa59b117bd5b8e0cc3aabd4583003
ShezHsky <shezhsky@icloud.com>
2022-08-18 20:33:12 +0100

    Use appropriate con slug for the pirate icon

--- EurofurenceTests/ApplicationTargetAppIconRepositoryTests.swift
+++ EurofurenceTests/ApplicationTargetAppIconRepositoryTests.swift
@@ -7,7 +7,7 @@ class ApplicationTargetAppIconRepositoryTests: XCTestCase {
     func testContainsExpectedIcons() {
         let expectedIconsWithinApplicationBundle: [AppIcon] = [
             .init(displayName: "Classic", imageFileName: "Classic", alternateIconName: "Classic"),
-            .init(displayName: "On the High Seas", imageFileName: "Pirate", alternateIconName: nil)
+            .init(displayName: "Welcome to Tortuga", imageFileName: "Pirate", alternateIconName: nil)
         ]
         
         let repository = ApplicationTargetAppIconRepository()
