ea90978c387d86581188c451455d4d765e789912
ShezHsky <shezhsky@icloud.com>
2022-09-02 07:24:49 +0100

    Fix compilation issue

--- Packages/EurofurenceKit/Tests/EurofurenceWebAPITests/CIDSensitiveEurofurenceAPITests.swift
+++ Packages/EurofurenceKit/Tests/EurofurenceWebAPITests/CIDSensitiveEurofurenceAPITests.swift
@@ -19,13 +19,14 @@ class CIDSensitiveEurofurenceAPITests: XCTestCase {
     func testSyncWithTimestampUsesExpectedURL() async throws {
         let network = FakeNetwork()
         let lastSyncTime = Date()
+        let generationToken = SynchronizationPayload.GenerationToken(lastSyncTime: lastSyncTime)
         let formattedSyncTime = EurofurenceISO8601DateFormatter.instance.string(from: lastSyncTime)
         let expectedURL = try XCTUnwrap(URL(string: "https://app.eurofurence.org/EF26/api/Sync?since=\(formattedSyncTime)"))
         let responseFileURL = try XCTUnwrap(Bundle.module.url(forResource: "EF26FullSyncResponse", withExtension: "json"))
         let responseFileData = try Data(contentsOf: responseFileURL)
         network.stub(url: expectedURL, with: .success(responseFileData))
         let api = CIDSensitiveEurofurenceAPI(network: network)
-        _ = try await api.fetchChanges(since: lastSyncTime)
+        _ = try await api.fetchChanges(since: generationToken)
         
         let expected = FakeNetwork.Event.get(url: expectedURL)
         XCTAssertEqual([expected], network.history)
