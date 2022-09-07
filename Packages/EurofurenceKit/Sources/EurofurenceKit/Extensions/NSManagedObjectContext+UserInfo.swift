64ab4fc50766ab2a87f9f37783eb1678c7d271b2
ShezHsky <shezhsky@icloud.com>
2022-09-07 08:46:57 +0100

    Extract constants

--- Packages/EurofurenceKit/Sources/EurofurenceKit/Extensions/NSManagedObjectContext+UserInfo.swift
+++ Packages/EurofurenceKit/Sources/EurofurenceKit/Extensions/NSManagedObjectContext+UserInfo.swift
@@ -5,42 +5,50 @@ import EurofurenceWebAPI
 
 extension NSManagedObjectContext {
     
+    private struct Keys {
+        static let persistentContainerKey = "EFKPersistentContainer"
+        static let propertiesKey = "EFKModelProperties"
+        static let keychainKey = "EFKKeychain"
+        static let apiKey = "EFKEurofurenceAPI"
+    }
+    
     /// The `NSPersistentContainer` that produced this managed object context.
     var persistentContainer: NSPersistentContainer? {
         get {
-            userInfo.object(forKey: "EFKPersistentContainerKey") as? NSPersistentContainer
+            userInfo.object(forKey: Keys.persistentContainerKey) as? NSPersistentContainer
         }
         set {
-            userInfo.setObject(newValue as Any, forKey: "EFKPersistentContainerKey" as NSCopying)
+            userInfo.setObject(newValue as Any, forKey: Keys.persistentContainerKey as NSCopying)
         }
     }
     
     /// The collection of properties in use by the model.
     var properties: EurofurenceModelProperties? {
         get {
-            userInfo.object(forKey: "EFKModelProperties") as? EurofurenceModelProperties
+            userInfo.object(forKey: Keys.propertiesKey) as? EurofurenceModelProperties
         }
         set {
-            userInfo.setObject(newValue as Any, forKey: "EFKModelProperties" as NSCopying)
+            userInfo.setObject(newValue as Any, forKey: Keys.propertiesKey as NSCopying)
         }
     }
     
+    /// The `Keychain` in use by the model, for fetching the credentials of the current user.
     var keychain: Keychain? {
         get {
-            userInfo.object(forKey: "EFKKeychain") as? Keychain
+            userInfo.object(forKey: Keys.keychainKey) as? Keychain
         }
         set {
-            userInfo.setObject(newValue as Any, forKey: "EFKKeychain" as NSCopying)
+            userInfo.setObject(newValue as Any, forKey: Keys.keychainKey as NSCopying)
         }
     }
     
     /// The instance of the `EurofurenceAPI` in use by the model.
     var eurofurenceAPI: EurofurenceAPI? {
         get {
-            userInfo.object(forKey: "EFKEurofurenceAPI") as? EurofurenceAPI
+            userInfo.object(forKey: Keys.apiKey) as? EurofurenceAPI
         }
         set {
-            userInfo.setObject(newValue as Any, forKey: "EFKEurofurenceAPI" as NSCopying)
+            userInfo.setObject(newValue as Any, forKey: Keys.apiKey as NSCopying)
         }
     }
     
