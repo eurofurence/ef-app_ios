c9ab745cc1d782b97175352e31df7a21970ea8eb
ShezHsky <shezhsky@icloud.com>
2022-09-07 14:53:12 +0100

    Copy current branding into shared package

---
+++ Packages/EurofurenceKit/Sources/EurofurenceKit/Branding/Color+Branding.swift
@@ -0,0 +1,23 @@
+import SwiftUI
+
+extension Color {
+    
+    /// Color used for large action buttons.
+    public static let largeActionButton = Color("Large Action Button")
+    
+    /// The Pantone 330U color
+    public static let pantone330U = Color("Pantone 330U")
+    
+    /// The Pantone 330U color at 45% full shade
+    public static let pantone330U_45 = Color("Pantone 330U (45%)")
+    
+    /// The Pantone 330U color at 26% full shade
+    public static let pantone330U_26 = Color("Pantone 330U (26%)")
+    
+    /// The Pantone 330U color at 13% full shade
+    public static let pantone330U_13 = Color("Pantone 330U (13%)")
+    
+    /// The Pantone 330U color at 5% full shade
+    public static let pantone330U_5 = Color("Pantone 330U (5%)")
+    
+}
