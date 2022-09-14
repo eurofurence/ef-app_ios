a9e2e003c35bbfbdd33cd38b7af7a8ef6e7cd624
ShezHsky <shezhsky@icloud.com>
2022-09-14 09:52:58 +0100

    Soften corners of images

--- Packages/EurofurenceKit/Sources/EurofurenceKit/SwiftUI/Views/EurofurenceKitImage.swift
+++ Packages/EurofurenceKit/Sources/EurofurenceKit/SwiftUI/Views/EurofurenceKitImage.swift
@@ -14,6 +14,7 @@ public struct EurofurenceKitImage: View {
             image
                 .resizable()
                 .aspectRatio(contentMode: .fit)
+                .cornerRadius(7)
         } placeholder: {
             Color.gray
         }
