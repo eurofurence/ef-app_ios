a046069d7a2ca26650e44b2073757ff793738f43
ShezHsky <shezhsky@icloud.com>
2022-09-15 08:01:11 +0100

    Implement device sensitive clock

--- Packages/EurofurenceKit/Sources/EurofurenceKit/Time/Clock.swift
+++ Packages/EurofurenceKit/Sources/EurofurenceKit/Time/Clock.swift
@@ -1,38 +1,14 @@
 import Combine
 import Foundation
 
+/// A type that adapts the system's temporal state.
 public protocol Clock {
     
+    /// A publisher that emits a `Date` whenever a significant change in time occurs.
     typealias SignificantTimeChangePublisher = CurrentValueSubject<Date, Never>
     
+    /// An combine pipeline that publishes values whenever a significant change in the current time occurs (e.g. change
+    /// of day).
     var significantTimeChangePublisher: SignificantTimeChangePublisher { get }
-    
-}
-
-public class DeviceSensitiveClock: Clock {
-    
-    public static let shared = DeviceSensitiveClock()
-    
-    private init() {
-        
-    }
-    
-    public let significantTimeChangePublisher = SignificantTimeChangePublisher(Date())
-    
-#if os(macOS)
-    @objc private func calendarDayChanged(_ notification: Notification) {
-        
-    }
-#endif
-    
-#if os(iOS)
-    @objc private func significantTimeChange(_ notification: Notification) {
-        
-    }
-#endif
-    
-    private func processSignificantTimeChange() {
-        
-    }
     
 }
