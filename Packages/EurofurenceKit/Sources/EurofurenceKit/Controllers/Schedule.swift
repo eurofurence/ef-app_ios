837bcdcf9709ae791c753c53f93ca9e2e99783db
ShezHsky <shezhsky@icloud.com>
2022-09-17 09:38:35 +0100

    Enable filtering schedule to favourites

--- Packages/EurofurenceKit/Sources/EurofurenceKit/Controllers/Schedule.swift
+++ Packages/EurofurenceKit/Sources/EurofurenceKit/Controllers/Schedule.swift
@@ -59,6 +59,13 @@ public class Schedule: NSObject, ObservableObject {
     /// A localized description of the filter.
     @Published public private(set) var localizedFilterDescription: String?
     
+    /// A flag to designate whether the schedule should only contain events the user has favourited.
+    @Published public var favouritesOnly: Bool = false {
+        didSet {
+            refetchEvents()
+        }
+    }
+    
     /// A textual query to apply for filtering against this schedule.
     @Published public var query: String = "" {
         didSet {
@@ -208,6 +215,10 @@ public class Schedule: NSObject, ObservableObject {
     
     private func makeFetchingPredicate() -> NSPredicate {
         var predicates = [NSPredicate]()
+        
+        if favouritesOnly {
+            predicates.append(NSPredicate(format: "isFavourite == YES"))
+        }
         
         if query.isEmpty == false {
             predicates.append(Event.predicateForTextualSearch(query: query))
