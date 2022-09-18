5fc8ba5bfe3d66c25e76f3f0b123cae21b3fb414
ShezHsky <shezhsky@icloud.com>
2022-09-18 23:31:03 +0100

    Implement feedback submission API endpoint

--- Packages/EurofurenceKit/Sources/EurofurenceWebAPI/Requests/SubmitEventFeedback.swift
+++ Packages/EurofurenceKit/Sources/EurofurenceWebAPI/Requests/SubmitEventFeedback.swift
@@ -1,3 +1,5 @@
+import Foundation
+
 extension APIRequests {
     
     public struct SubmitEventFeedback: APIRequest {
@@ -19,7 +21,19 @@ extension APIRequests {
         public typealias Output = Void
         
         public func execute(with context: APIRequestExecutionContext) async throws {
+            let url = context.makeURL(subpath: "EventFeedback")
+            let bodyContents = SubmitFeedbackContents(EventId: identifier, Rating: rating, Message: additionalComments)
+            let encoder = JSONEncoder()
+            let body = try encoder.encode(bodyContents)
+            let request = NetworkRequest(url: url, body: body, method: .post)
             
+            try await context.network.perform(request: request)
+        }
+        
+        private struct SubmitFeedbackContents: Encodable {
+            var EventId: String
+            var Rating: Int
+            var Message: String
         }
         
     }
