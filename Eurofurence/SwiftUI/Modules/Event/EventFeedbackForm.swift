e8ba662c112c0e7c13be1a4d1a85cb4b6ae069fd
ShezHsky <shezhsky@icloud.com>
2022-09-21 08:28:13 +0100

    Extract resource

--- Eurofurence/SwiftUI/Modules/Event/EventFeedbackForm.swift
+++ Eurofurence/SwiftUI/Modules/Event/EventFeedbackForm.swift
@@ -1,20 +1,5 @@
 import EurofurenceKit
 import SwiftUI
-#if canImport(UIKit)
-import UIKit
-#endif
-
-extension Color {
-    
-    static var formHeading: Color {
-#if canImport(UIKit)
-        return Color(uiColor: .systemGroupedBackground)
-#else
-        return .clear
-#endif
-    }
-    
-}
 
 struct EventFeedbackForm: View {
     
